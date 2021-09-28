a7462457b195cae6788e7328428bcc13eef1bbfc
Vikas Patil <patilvikasj@gmail.com>
2021-09-28 20:53:32 +0530

    Added prefix for Gutenberg editor style for links

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'a7cc38efb8f5a5734d5b532a0a0a5c9f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '570722befb9798f9b3ce49035419e85b');

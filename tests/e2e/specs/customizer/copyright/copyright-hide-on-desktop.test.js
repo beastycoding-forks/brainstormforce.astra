79dd0248ffaeb04ff317e0da0bdd2be62fab0c91
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2021-10-05 14:25:53 +0530

    Update copyright-hide-on-desktop.test.js

--- tests/e2e/specs/customizer/copyright/copyright-hide-on-desktop.test.js
+++ tests/e2e/specs/customizer/copyright/copyright-hide-on-desktop.test.js
@@ -1,18 +1,18 @@
 import { createURL, setBrowserViewport } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../utils/set-customize';
 describe( 'copyright hide on desktop settings in the customizer', () => {
-    it( 'copyright hide on desktop setting should apply correctly', async () => {
-        const copyrighthideonDesktop = {
-            'section-footer-copyright-hide-desktop': 'grid', 
+	it( 'copyright hide on desktop setting should apply correctly', async () => {
+		const copyrighthideonDesktop = {
+			'section-footer-copyright-hide-desktop': 'grid', 
         };
-        await setCustomize( copyrighthideonDesktop );
-        await page.goto( createURL( '/' ), {
-        waitUntil: 'networkidle0',
-        } );
-        await page.evaluate( () => {
+		await setCustomize( copyrighthideonDesktop );
+		await page.goto( createURL( '/' ), {
+		waitUntil: 'networkidle0',
+		} );
+		await page.evaluate( () => {
             window.scrollBy(0, window.innerHeight);
         });
-        await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder' );
+		 await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder' );
 
         await setBrowserViewport( 'large' );
         await expect( {

52bdf6964444e2709467ff6c09df946537257420
Snehal Kolhe <snehalk@bsf.io>
2021-10-06 18:22:45 +0530

    Added test cases for button block and heading block

--- tests/e2e/specs/block-editor/button-block/button.test.js
+++ tests/e2e/specs/block-editor/button-block/button.test.js
@@ -1,12 +1,18 @@
-import { setPostContent, createNewPost } from '@wordpress/e2e-test-utils';
-import { GUTENBERG_TEST_BUTTON_BLOCK } from '../../../utils/button-block';
+import { insertBlock, createNewPost, pressKeyWithModifier } from '@wordpress/e2e-test-utils';
 describe( 'Button in gutenberg editor', () => {
 	it( 'login logout button should display', async () => {
 		await createNewPost( {
 			postType: 'post',
 			title: 'test Gutenberg button',
 		} );
-		await setPostContent( GUTENBERG_TEST_BUTTON_BLOCK );
+		await insertBlock( 'Buttons' );
+		await page.keyboard.type( 'Login' );
+		await pressKeyWithModifier( 'primary', 'k' );
+		await page.waitForFunction(
+			() => !! document.activeElement.closest( '.block-editor-url-input' ),
+		);
+		await page.keyboard.type( 'https://google.com' );
+	    await page.keyboard.press( 'Enter' );
 		await page.waitForSelector( '.edit-post-visual-editor .block-editor-block-list__block' );
 		await expect( {
 			selector: '.wp-block-buttons',
@@ -33,5 +39,26 @@ describe( 'Button in gutenberg editor', () => {
 			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link',
 			property: 'padding-left',
 		} ).cssValueToBe( `20px` );
+
+		await page.waitForSelector( '.wp-block-buttons .wp-block-button .wp-block-button__link' );
+		await expect( {
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link',
+			property: 'border-top-width',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.wp-block-buttons .wp-block-button .wp-block-button__link' );
+		await expect( {
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link',
+			property: 'border-right-width',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.wp-block-buttons .wp-block-button .wp-block-button__link' );
+		await expect( {
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link',
+			property: 'border-left-width',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.wp-block-buttons .wp-block-button .wp-block-button__link' );
+		await expect( {
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link',
+			property: 'border-bottom-width',
+		} ).cssValueToBe( `0px` );
 	} );
 } );
---
+++ tests/e2e/specs/block-editor/heading-block/heading.test.js
@@ -0,0 +1,37 @@
+import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
+describe( 'Heading in gutenberg editor', () => {
+	it( 'heading should display', async () => {
+		await createNewPost( {
+			postType: 'post',
+			title: 'test Heading',
+		} );
+		await insertBlock( 'Heading' );
+		await page.keyboard.type( 'Gutenberg Heading Test' );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'max-width',
+		} ).cssValueToBe( `1200px` );
+        await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.editor-styles-wrapper h2',
+			property: 'margin-top',
+		} ).cssValueToBe( `24.9px` );
+        await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.editor-styles-wrapper h2',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `24.9px` );
+        await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'padding-left',
+		} ).cssValueToBe( `0px` );
+        await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading ',
+			property: 'padding-right',
+		} ).cssValueToBe( `0px` );
+        
+	} );
+} );

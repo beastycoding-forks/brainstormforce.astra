196d97f420a39a013efcd522a824aac704a4275b
akshaylakh <akshayl@bsf.io>
2021-10-06 12:58:40 +0530

    fixed inting errors

--- tests/e2e/specs/customizer/sidebar/rightSidebar.js
+++ tests/e2e/specs/customizer/sidebar/rightSidebar.js
@@ -4,61 +4,58 @@ import {
 	publishPost,
 } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../utils/set-customize';
-describe( 'To test Right Sidebar size in the customizer ', () => {
-    it( 'Right Sidebar size should apply correctly on post', async () => {
-        const postRightSidebarSize = {
-            'site-sidebar-layout': 'right-sidebar',
-            'site-post-sidebar-layout': 'right-sidebar',
-            'inspector-input-control-0' : '35%'
-        };
-        await setCustomize( postRightSidebarSize );
-        await createNewPost( { postType: 'post', title: 'sidebar post' } );
-        await publishPost();
-        await page.goto( createURL( '/sidebar-post/' ), {
-            waitUntil: 'networkidle0',
-        } );
-        await page.waitForSelector( '.ast-separate-container.ast-right-sidebar #secondary' );
-
-        await expect( {
-            selector: '#secondary',
-            property: 'width',
-        } ).cssValueToBe( `${ postRightSidebarSize[ 'inspector-input-control-0' ] }` );
-
-    } );
-
-    it( 'Right Sidebar size should apply correctly on page', async () => {
-        const pageRightSidebarSize = {
-            'site-sidebar-layout': 'right-sidebar',
-            'site-page-sidebar-layout': 'right-sidebar',
-            'inspector-input-control-0' : '35%'
-        };
-        await setCustomize( pageRightSidebarSize );
-        await createNewPost( { postType: 'page', title: 'sidebar page' } );
-        await publishPost();
-        await page.goto( createURL( '/sidebar-page/' ), {
-            waitUntil: 'networkidle0',
-        } );
-        await page.waitForSelector( '.ast-separate-container.ast-right-sidebar #secondary' );
-        await expect( {
-            selector: '#secondary',
-            property: 'width',
-        } ).cssValueToBe( `${ pageRightSidebarSize[ 'inspector-input-control-0' ] }` );
-    });
-
-    it( 'Right Sidebar size should apply correctly on archive archive posts', async () => {
-        const archiveRightSidebarSize = {
-            'site-sidebar-layout': 'right-sidebar',
-            'archive-post-sidebar-layout': 'right-sidebar',
-            'inspector-input-control-0' : '35%'
-        };
-        await setCustomize( archiveRightSidebarSize );
-        await page.goto( createURL( '/category/uncategorized' ), {
-            waitUntil: 'networkidle0',
-        } );
-        await page.waitForSelector( '.ast-separate-container.ast-right-sidebar #secondary' );
-        await expect( {
-            selector: '#secondary',
-            property: 'width',
-        } ).cssValueToBe( `${ archiveRightSidebarSize[ 'inspector-input-control-0' ] }` );
-    });
+describe( 'to test Right Sidebar size in the customizer', () => {
+	it( 'right Sidebar size should apply correctly on post', async () => {
+		const postRightSidebarSize = {
+			'site-sidebar-layout': 'right-sidebar',
+			'site-post-sidebar-layout': 'right-sidebar',
+			'inspector-input-control-0': '35%',
+		};
+		await setCustomize( postRightSidebarSize );
+		await createNewPost( { postType: 'post', title: 'sidebar post' } );
+		await publishPost();
+		await page.goto( createURL( '/sidebar-post/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-separate-container.ast-right-sidebar #secondary' );
+		await expect( {
+			selector: '#secondary',
+			property: 'width',
+		} ).cssValueToBe( `${ postRightSidebarSize[ 'inspector-input-control-0' ] }` );
+	} );
+	it( 'right Sidebar size should apply correctly on page', async () => {
+		const pageRightSidebarSize = {
+			'site-sidebar-layout': 'right-sidebar',
+			'site-page-sidebar-layout': 'right-sidebar',
+			'inspector-input-control-0': '35%',
+		};
+		await setCustomize( pageRightSidebarSize );
+		await createNewPost( { postType: 'page', title: 'sidebar page' } );
+		await publishPost();
+		await page.goto( createURL( '/sidebar-page/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-separate-container.ast-right-sidebar #secondary' );
+		await expect( {
+			selector: '#secondary',
+			property: 'width',
+		} ).cssValueToBe( `${ pageRightSidebarSize[ 'inspector-input-control-0' ] }` );
+	} );
+	it( 'right Sidebar size should apply correctly on archive archive posts', async () => {
+		const archiveRightSidebarSize = {
+			'site-sidebar-layout': 'right-sidebar',
+			'archive-post-sidebar-layout': 'right-sidebar',
+			'inspector-input-control-0': '35%',
+		};
+		await setCustomize( archiveRightSidebarSize );
+		await page.goto( createURL( '/category/uncategorized' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-separate-container.ast-right-sidebar #secondary' );
+		await expect( {
+			selector: '#secondary',
+			property: 'width',
+		} ).cssValueToBe(
+			`${ archiveRightSidebarSize[ 'inspector-input-control-0' ] }` );
+	} );
 } );

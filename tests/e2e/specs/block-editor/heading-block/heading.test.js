d9dcfd0d4db04b28c9c95f04c6e9cbdef3828233
Snehal Kolhe <snehalk@bsf.io>
2021-10-06 18:41:23 +0530

    code indentation of heading block file

--- tests/e2e/specs/block-editor/heading-block/heading.test.js
+++ tests/e2e/specs/block-editor/heading-block/heading.test.js
@@ -12,26 +12,25 @@ describe( 'Heading in gutenberg editor', () => {
 			selector: '.wp-block-heading',
 			property: 'max-width',
 		} ).cssValueToBe( `1200px` );
-        await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
 		await expect( {
 			selector: '.editor-styles-wrapper h2',
 			property: 'margin-top',
 		} ).cssValueToBe( `24.9px` );
-        await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
 		await expect( {
 			selector: '.editor-styles-wrapper h2',
 			property: 'margin-bottom',
 		} ).cssValueToBe( `24.9px` );
-        await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
 		await expect( {
 			selector: '.wp-block-heading',
 			property: 'padding-left',
 		} ).cssValueToBe( `0px` );
-        await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
 		await expect( {
 			selector: '.wp-block-heading ',
 			property: 'padding-right',
 		} ).cssValueToBe( `0px` );
-        
 	} );
 } );

73e55578cf6dae45a56e58637fc1ece034038ca1
Snehal Kolhe <snehalk@bsf.io>
2021-10-07 18:24:39 +0530

    Test case for GB heading block padding

---
+++ tests/e2e/specs/block-editor/heading-block/heading-padding.test.js
@@ -0,0 +1,31 @@
+import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
+describe( 'Heading in gutenberg editor', () => {
+	it( 'heading property should apply correctly', async () => {
+		await createNewPost( {
+			postType: 'post',
+			title: 'test Heading',
+		} );
+		await insertBlock( 'Heading' );
+		await page.keyboard.type( 'Gutenberg Heading Test' );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'padding-left',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading ',
+			property: 'padding-right',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading ',
+			property: 'padding-top',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading ',
+			property: 'padding-bottom',
+		} ).cssValueToBe( `0px` );
+	} );
+} );

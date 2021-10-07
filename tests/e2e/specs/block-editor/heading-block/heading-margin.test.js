c1b64d64e4bd1a96d0177325a9d4c1c73d80477b
Snehal Kolhe <snehalk@bsf.io>
2021-10-07 17:58:15 +0530

    E2E test case for Gutenberg heading block margin

---
+++ tests/e2e/specs/block-editor/heading-block/heading-margin.test.js
@@ -0,0 +1,31 @@
+import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
+describe( 'Heading in gutenberg editor', () => {
+	it( 'assert margin of the heading in the block editor', async () => {
+		await createNewPost( {
+			postType: 'post',
+			title: 'test Heading',
+		} );
+		await insertBlock( 'Heading' );
+		await page.keyboard.type( 'Gutenberg Heading Test' );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.editor-styles-wrapper h2',
+			property: 'margin-top',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.editor-styles-wrapper h2',
+			property: 'margin-right',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.editor-styles-wrapper h2',
+			property: 'margin-left',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.editor-styles-wrapper h2',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `24.9px` );
+	} );
+} );

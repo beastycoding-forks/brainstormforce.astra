3d6d1486a4d002cab5c1222991805d56d9ff8c6c
Simran <simranc@bsf.io>
2021-10-10 12:06:38 +0530

    Test case for Below footer inner element layout

---
+++ tests/e2e/specs/customizer/below-footer/inner-element-layout.test.js
@@ -0,0 +1,49 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/set-customize';
+describe( 'Below footer inner element layout setting in customizer', () => {
+	it( 'stack layout should apply correctly', async () => {
+		const innerelemetLayout = {
+			'hbb-stack': {
+				desktop: 'stack',
+		        tablet: 'stack',
+		        mobile: 'stack',
+			},
+		};
+		await setCustomize( innerelemetLayout );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.evaluate( () => {
+			window.scrollBy( 0, window.innerHeight );
+		} );
+		await page.waitForSelector( '.site-below-footer-wrap' );
+		await expect( {
+			selector: '.site-below-footer-wrap',
+			property: '',
+		} ).cssValueToBe( ``,
+		);
+	} );
+
+	it( 'inline layout should apply correctly', async () => {
+		const innerelemetLayout = {
+			'hbb-stack': {
+				desktop: 'inline',
+		        tablet: 'inline',
+		        mobile: 'inline',
+			},
+		};
+		await setCustomize( innerelemetLayout );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.evaluate( () => {
+			window.scrollBy( 0, window.innerHeight );
+		} );
+		await page.waitForSelector( '.site-below-footer-wrap' );
+		await expect( {
+			selector: '.site-below-footer-wrap',
+			property: '',
+		} ).cssValueToBe( ``,
+		);
+	} );
+} );

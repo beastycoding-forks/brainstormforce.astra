7f0db391f0c9f71bd39033796c1b9440c131c7fb
Simran <simranc@bsf.io>
2021-10-11 15:41:39 +0530

    Added test case for gutenberg quote block for margin and padding

---
+++ tests/e2e/specs/block-editor/quote-block/margin-padding.test.js
@@ -0,0 +1,56 @@
+/**
+ * WordPress dependencies
+ */
+import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
+
+describe( 'Quote', () => {
+	it( 'padding, margin should apply properly of the quote in the block editor', async () => {
+		await createNewPost( {
+			postType: 'post',
+			title: 'test Quote',
+		} );
+		await insertBlock( 'Quote' );
+		await page.keyboard.type( '> A quote' );
+
+		await page.waitForSelector( '.edit-post-visual-editor .block-editor-block-list__block' );
+		await expect( {
+			selector: '.edit-post-visual-editor blockquote',
+			property: 'padding-left',
+		} ).cssValueToBe( `19.8px` );
+
+		await expect( {
+			selector: '.edit-post-visual-editor blockquote',
+			property: 'padding-right',
+		} ).cssValueToBe( `19.8px` );
+
+		await expect( {
+			selector: '.edit-post-visual-editor blockquote',
+			property: 'padding-top',
+		} ).cssValueToBe( `19.8px` );
+
+		await expect( {
+			selector: '.edit-post-visual-editor blockquote',
+			property: 'padding-bottom',
+		} ).cssValueToBe( `19.8px` );
+
+		await expect( {
+			selector: '.edit-post-visual-editor blockquote',
+			property: 'margin-left',
+		} ).cssValueToBe( `0px` );
+
+		await expect( {
+			selector: '.edit-post-visual-editor blockquote',
+			property: 'margin-right',
+		} ).cssValueToBe( `0px` );
+
+		await expect( {
+			selector: '.edit-post-visual-editor blockquote',
+			property: 'margin-top',
+		} ).cssValueToBe( `24.75px` );
+
+		await expect( {
+			selector: '.edit-post-visual-editor blockquote',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `24.75px` );
+	} );
+} );

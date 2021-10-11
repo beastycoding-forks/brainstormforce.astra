500a574fb683d77045d4542f287fbc991daad697
Snehal Kolhe <snehalk@bsf.io>
2021-10-11 11:49:42 +0530

    E2E test case for GB Heading Wide width

--- tests/e2e/specs/block-editor/heading-block/heading-wide-width.js
+++ tests/e2e/specs/block-editor/heading-block/heading-wide-width.js
@@ -1,25 +1,16 @@
-import { insertBlock, createNewPost, clickBlockToolbarButton } from '@wordpress/e2e-test-utils';
+import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
 describe( 'Heading in gutenberg editor', () => {
-	it( 'assert width of the heading in the block editor', async () => {
+	it( 'assert wide width of the heading in the block editor', async () => {
 		await createNewPost( {
 			postType: 'post',
 			title: 'test Heading',
 		} );
 		await insertBlock( 'Heading' );
-		await page.keyboard.type( 'Gutenberg Heading Test' );
-		await clickBlockToolbarButton( 'Align' );
-		await page.waitForFunction( () =>
-			document.activeElement.classList.contains(
-				'components-dropdown-menu__menu-item',
-			),
-		);
-		await page.click(
-			'[aria-label="Align"] button:nth-child(1)',
-		);
+		await page.keyboard.type( 'Heading Block' );
 		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
 		await expect( {
 			selector: '.wp-block-heading',
 			property: 'max-width',
-		} ).cssValueToBe( `1256px` );
+		} ).cssValueToBe( `1200px` );
 	} );
 } );

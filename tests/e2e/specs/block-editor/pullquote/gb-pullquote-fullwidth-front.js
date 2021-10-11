b56577a29c90703b9208179ec3a695369f510c55
akshaylakh <68679840+akshaylakh@users.noreply.github.com>
2021-10-11 13:57:40 +0530

    Updated file name & changed size

--- tests/e2e/specs/block-editor/pullquote/gb-pullquote-fullwidth-front.js
+++ tests/e2e/specs/block-editor/pullquote/gb-pullquote-fullwidth-front.js
@@ -28,7 +28,7 @@ describe( 'Pullquote in gutenberg editor', () => {
 		await expect( {
 			selector: 'blockquote',
 			property: 'width',
-		} ).cssValueToBe( `868.9px` );
+		} ).cssValueToBe( `194.672px` );
 		// to test the padding
 		await expect( {
 			selector: 'blockquote',

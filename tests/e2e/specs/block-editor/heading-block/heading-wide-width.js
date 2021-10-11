d0a0ee0178e4eb430797b28b2f505557c9a7713e
Snehal Kolhe <snehalk@bsf.io>
2021-10-11 12:17:24 +0530

    margin and padding test cases added

--- tests/e2e/specs/block-editor/heading-block/heading-wide-width.js
+++ tests/e2e/specs/block-editor/heading-block/heading-wide-width.js
@@ -1,16 +1,59 @@
 import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
 describe( 'Heading in gutenberg editor', () => {
-	it( 'assert wide width of the heading in the block editor', async () => {
+	it( 'assert wide width & padding, margin of the heading in the block editor', async () => {
 		await createNewPost( {
 			postType: 'post',
 			title: 'test Heading',
 		} );
 		await insertBlock( 'Heading' );
 		await page.keyboard.type( 'Heading Block' );
+
 		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
 		await expect( {
 			selector: '.wp-block-heading',
 			property: 'max-width',
 		} ).cssValueToBe( `1200px` );
+
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'padding-left',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'padding-right',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'padding-top',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'padding-bottom',
+		} ).cssValueToBe( `0px` );
+
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'margin-top',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'margin-right',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `24.9px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'margin-left',
+		} ).cssValueToBe( `0px` );
 	} );
 } );

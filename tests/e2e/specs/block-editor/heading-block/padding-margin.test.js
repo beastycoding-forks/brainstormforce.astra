f5ca7c9ef29f2a66c0e19c396fb4723ace9e6a2d
Snehal Kolhe <snehalk@bsf.io>
2021-10-11 09:37:58 +0530

    heading block padding test case code added

--- tests/e2e/specs/block-editor/heading-block/padding-margin.test.js
+++ tests/e2e/specs/block-editor/heading-block/padding-margin.test.js
@@ -28,4 +28,32 @@ describe( 'Heading in gutenberg editor', () => {
 			property: 'margin-bottom',
 		} ).cssValueToBe( `24.9px` );
 	} );
+	it( 'assert padding property of the heading in the block editor', async () => {
+		await createNewPost( {
+			postType: 'post',
+			title: 'test Heading',
+		} );
+		await insertBlock( 'Heading' );
+		await page.keyboard.type( 'Gutenberg Heading Test' );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading',
+			property: 'padding-left',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading ',
+			property: 'padding-right',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading ',
+			property: 'padding-top',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .wp-block-heading' );
+		await expect( {
+			selector: '.wp-block-heading ',
+			property: 'padding-bottom',
+		} ).cssValueToBe( `0px` );
+	} );
 } );

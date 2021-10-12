c32673133b11e8aa59836aa7ef872715b51c6764
Simran <simranc@bsf.io>
2021-10-12 18:07:43 +0530

    Test case for Gutenberg table block full and wide width

---
+++ tests/e2e/specs/block-editor/table-block/table-wide-width.test.js
@@ -0,0 +1,87 @@
+import {
+	clickButton,
+	createNewPost,
+	getEditedPostContent,
+	insertBlock,
+    clickBlockToolbarButton,
+} from '@wordpress/e2e-test-utils';
+
+const createButtonLabel = 'Create Table';
+
+describe( 'Table', () => {
+	beforeEach( async () => {
+		await createNewPost();
+	} );
+
+	it( 'displays a form for choosing the row and column count of the table', async () => {
+		await insertBlock( 'Table' );
+
+		// Check for existence of the column count field.
+		const columnCountLabel = await page.$x(
+			"//figure[@data-type='core/table']//label[text()='Column count']"
+		);
+		expect( columnCountLabel ).toHaveLength( 1 );
+
+		// Modify the column count.
+		await columnCountLabel[ 0 ].click();
+		const currentColumnCount = await page.evaluate(
+			() => document.activeElement.value
+		);
+		expect( currentColumnCount ).toBe( '2' );
+		await page.keyboard.press( 'Backspace' );
+		await page.keyboard.type( '5' );
+
+		// Check for existence of the row count field.
+		const rowCountLabel = await page.$x(
+			"//figure[@data-type='core/table']//label[text()='Row count']"
+		);
+		expect( rowCountLabel ).toHaveLength( 1 );
+
+		// Modify the row count.
+		await rowCountLabel[ 0 ].click();
+		const currentRowCount = await page.evaluate(
+			() => document.activeElement.value
+		);
+		expect( currentRowCount ).toBe( '2' );
+		await page.keyboard.press( 'Backspace' );
+		await page.keyboard.type( '10' );
+
+		// Create the table.
+		await clickButton( createButtonLabel );
+
+		// Expect the post content to have a correctly sized table.
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+        await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			document.activeElement.classList.contains(
+				'components-dropdown-menu__menu-item',
+			),
+		);
+		await page.click(
+			'[aria-label="Align"] button:nth-child(4)',
+		);
+		await page.waitForSelector( '.block-editor-block-list__block' );
+		await expect( {
+			selector: '.block-editor-block-list__block',
+			property: 'width',
+		} ).cssValueToBe(
+			`974.9px`,
+		);
+		await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			document.activeElement.classList.contains(
+				'components-dropdown-menu__menu-item',
+			),
+		);
+		await page.click(
+			'[aria-label="Align"] button:nth-child(5)',
+		);
+		await page.waitForSelector( '.block-editor-block-list__block' );
+		await expect( {
+			selector: '.block-editor-block-list__block',
+			property: 'width',
+		} ).cssValueToBe(
+			`974.9px`,
+		);
+	} );
+} );

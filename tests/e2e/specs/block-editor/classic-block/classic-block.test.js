58c8d76ece7ae225d49a0681fb0e6c919d6d0185
Snehal Kolhe <snehalk@bsf.io>
2021-10-13 18:26:22 +0530

    e2e test case for Classic block default width, padding, margin

---
+++ tests/e2e/specs/block-editor/classic-block/classic-block.test.js
@@ -0,0 +1,102 @@
+import path from 'path';
+import fs from 'fs';
+import os from 'os';
+import { v4 as uuid } from 'uuid';
+
+/**
+ * WordPress dependencies
+ */
+import {
+	createNewPost,
+	insertBlock,
+} from '@wordpress/e2e-test-utils';
+
+describe( 'Classic', () => {
+	it( 'should insert media & assert width', async () => {
+		await createNewPost( {
+			postType: 'post',
+			title: 'classic block',
+		} );
+		await insertBlock( 'Classic' );
+		await page.waitForSelector( '.mce-content-body' );
+		await page.focus( '.mce-content-body' );
+		await page.keyboard.type( 'test' );
+
+		await page.waitForSelector( 'div[aria-label^="Add Media"]' );
+		await page.click( 'div[aria-label^="Add Media"]' );
+
+		await page.click( '.media-menu-item#menu-item-gallery' );
+		await page.waitForSelector( '.media-modal input[type=file]' );
+		const inputElement = await page.$( '.media-modal input[type=file]' );
+		const testImagePath = path.join(
+			__dirname,
+			'..',
+			'..',
+			'..',
+			'assets',
+			'pic.png',
+		);
+		const filename = uuid();
+		const tmpFileName = path.join( os.tmpdir(), filename + '.png' );
+		fs.copyFileSync( testImagePath, tmpFileName );
+		await inputElement.uploadFile( tmpFileName );
+
+		await page.waitForSelector(
+			`.media-modal li[aria-label="${ filename }"]`,
+		);
+		await page.click( '.media-modal button.media-button-gallery' );
+		await page.click( '.media-modal button.media-button-insert' );
+		await page.waitForSelector( '.mce-content-body img' );
+		await page.waitForSelector( '.block-editor-block-list__block' );
+		await expect( {
+			selector: '.edit-post-visual-editor .block-editor-block-list__block',
+			property: 'width',
+		} ).cssValueToBe( `975.235px` );
+	} );
+
+	it( 'assert padding of the gutenberg Classic block', async () => {
+		await page.waitForSelector( '.edit-post-visual-editor .block-editor-block-list__block' );
+		await expect( {
+			selector: '.edit-post-visual-editor .block-editor-block-list__block',
+			property: 'padding-top',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .block-editor-block-list__block' );
+		await expect( {
+			selector: '.edit-post-visual-editor .block-editor-block-list__block',
+			property: 'padding-right',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .block-editor-block-list__block' );
+		await expect( {
+			selector: '.edit-post-visual-editor .block-editor-block-list__block',
+			property: 'padding-left',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .block-editor-block-list__block' );
+		await expect( {
+			selector: '.edit-post-visual-editor .block-editor-block-list__block',
+			property: 'padding-bottom',
+		} ).cssValueToBe( `0px` );
+	} );
+
+	it( 'assert margin of the gutenberg classic block', async () => {
+		await page.waitForSelector( '.edit-post-visual-editor .block-editor-block-list__block' );
+		await expect( {
+			selector: '.edit-post-visual-editor .block-editor-block-list__block',
+			property: 'margin-top',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .block-editor-block-list__block' );
+		await expect( {
+			selector: '.edit-post-visual-editor .block-editor-block-list__block',
+			property: 'margin-right',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .block-editor-block-list__block' );
+		await expect( {
+			selector: '.edit-post-visual-editor .block-editor-block-list__block',
+			property: 'margin-left',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.edit-post-visual-editor .block-editor-block-list__block' );
+		await expect( {
+			selector: '.edit-post-visual-editor .block-editor-block-list__block',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `0px` );
+	} );
+} );

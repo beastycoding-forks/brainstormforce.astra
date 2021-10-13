ddcfbca01099f6a80c2978a8968e535c3317dd87
akshaylakh <akshayl@bsf.io>
2021-10-13 12:18:21 +0530

    added cases for default style

---
+++ tests/e2e/specs/block-editor/separator/default-style/sep-center-edit.js
@@ -0,0 +1,31 @@
+/**
+ * WordPress dependencies
+ */
+import {
+	insertBlock,
+	createNewPost,
+	clickBlockToolbarButton,
+} from '@wordpress/e2e-test-utils';
+
+describe( 'Separator in gutenberg editor', () => {
+	it( 'test separartor in the block editor', async () => {
+		await createNewPost( {
+			postType: 'post',
+			title: 'test separator',
+		} );
+		await insertBlock( 'Separator' );
+		await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			// eslint-disable-next-line @wordpress/no-global-active-element
+			document.activeElement.classList.contains(
+				'components-dropdown-menu__menu-item',
+			),
+		);
+		await page.click( '[aria-label="Align"] button:nth-child(1)' );
+		//to test width of separator
+		await expect( {
+			selector: '.editor-styles-wrapper .block-editor-block-list__layout.is-root-container > *',
+			property: 'max-width',
+		} ).cssValueToBe( `1200px` );
+	} );
+} );
---
+++ tests/e2e/specs/block-editor/separator/default-style/sep-fw-editor.js
@@ -0,0 +1,31 @@
+/**
+ * WordPress dependencies
+ */
+import {
+	insertBlock,
+	createNewPost,
+	clickBlockToolbarButton,
+} from '@wordpress/e2e-test-utils';
+
+describe( 'Separator in gutenberg editor', () => {
+	it( 'test separartor in the block editor', async () => {
+		await createNewPost( {
+			postType: 'post',
+			title: 'test separator',
+		} );
+		await insertBlock( 'Separator' );
+		await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			// eslint-disable-next-line @wordpress/no-global-active-element
+			document.activeElement.classList.contains(
+				'components-dropdown-menu__menu-item',
+			),
+		);
+		await page.click( '[aria-label="Align"] button:nth-child(2)' );
+		//to test width of separator
+		await expect( {
+			selector: '.wp-block',
+			property: 'width',
+		} ).cssValueToBe( '1119px' );
+	} );
+} );

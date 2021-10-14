5477051f074e2298e5fa6f59e85f7ff84c512071
Snehal Kolhe <snehalk@bsf.io>
2021-10-14 14:44:08 +0530

    e2e test case for default width, margin, padding for GB Classic block

---
+++ tests/e2e/specs/block-editor/custom-html-block/custom-html.test.js
@@ -0,0 +1,60 @@
+import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
+describe( 'custom html block in the gutenberg editor', () => {
+	it( 'assert default width of the custom html in the block editor', async () => {
+		await createNewPost( {
+			postType: 'post',
+			title: 'html',
+		} );
+		await insertBlock( 'Custom HTML' );
+
+		await page.waitForSelector( '.block-library-html__edit .block-editor-plain-text' );
+		await expect( {
+			selector: '.block-library-html__edit .block-editor-plain-text',
+			property: 'width',
+		} ).cssValueToBe( `975.235px` );
+	} );
+	it( 'assert margin of the custom html in the block editor', async () => {
+		await page.waitForSelector( '.block-library-html__edit .block-editor-plain-text' );
+		await expect( {
+			selector: '.block-library-html__edit .block-editor-plain-text',
+			property: 'margin-top',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.block-library-html__edit .block-editor-plain-text' );
+		await expect( {
+			selector: '.block-library-html__edit .block-editor-plain-text',
+			property: 'margin-left',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.block-library-html__edit .block-editor-plain-text' );
+		await expect( {
+			selector: '.block-library-html__edit .block-editor-plain-text',
+			property: 'margin-right',
+		} ).cssValueToBe( `0px` );
+		await page.waitForSelector( '.block-library-html__edit .block-editor-plain-text' );
+		await expect( {
+			selector: '.block-library-html__edit .block-editor-plain-text',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `0px` );
+	} );
+	it( 'assert padding of the custom html in the block editor', async () => {
+		await page.waitForSelector( '.block-library-html__edit .block-editor-plain-text' );
+		await expect( {
+			selector: '.block-library-html__edit .block-editor-plain-text',
+			property: 'padding-left',
+		} ).cssValueToBe( `13px` );
+		await page.waitForSelector( '.block-library-html__edit .block-editor-plain-text' );
+		await expect( {
+			selector: '.block-library-html__edit .block-editor-plain-text',
+			property: 'padding-right',
+		} ).cssValueToBe( `13px` );
+		await page.waitForSelector( '.block-library-html__edit .block-editor-plain-text' );
+		await expect( {
+			selector: '.block-library-html__edit .block-editor-plain-text',
+			property: 'padding-top',
+		} ).cssValueToBe( `10.4px` );
+		await page.waitForSelector( '.block-library-html__edit .block-editor-plain-text' );
+		await expect( {
+			selector: '.block-library-html__edit .block-editor-plain-text',
+			property: 'padding-bottom',
+		} ).cssValueToBe( `10.4px` );
+	} );
+} );

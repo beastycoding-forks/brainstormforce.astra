675af5a57f5a95a35bfbc021bdd6a292b4f71152
akshaylakh <68679840+akshaylakh@users.noreply.github.com>
2021-10-14 11:50:21 +0530

    updated describe & it statement

--- tests/e2e/specs/block-editor/separator/wide-line/sep-wide-ww-editor.js
+++ tests/e2e/specs/block-editor/separator/wide-line/sep-wide-ww-editor.js
@@ -8,8 +8,8 @@ import {
 	openDocumentSettingsSidebar,
 } from '@wordpress/e2e-test-utils';
 
-describe( 'Separator in gutenberg editor', () => {
-	it( 'test separartor in the block editor', async () => {
+describe( 'Separator in gutenberg editor for wide line style', () => {
+	it( 'test separartor in the block editor for wide width alignment', async () => {
 		await createNewPost( {
 			postType: 'post',
 			title: 'test separator',

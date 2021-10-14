c14bf1007bcb97925b26d9587a26d64448680800
Simran <simranc@bsf.io>
2021-10-14 12:35:22 +0530

    Test case for Gutenberg preformatted block margin and padding

---
+++ tests/e2e/specs/block-editor/preformatted-block/preformatted.test.js
@@ -0,0 +1,57 @@
+import {
+	getEditedPostContent,
+	createNewPost,
+	insertBlock,
+} from '@wordpress/e2e-test-utils';
+
+describe( 'Preformatted', () => {
+	beforeEach( async () => {
+		await createNewPost();
+	} );
+	it( 'should preserve white space when merging', async () => {
+		await insertBlock( 'Preformatted' );
+		await page.keyboard.type( '1' );
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( '2' );
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.press( 'ArrowDown' );
+		await page.keyboard.type( '3' );
+		await page.keyboard.press( 'ArrowLeft' );
+		await page.keyboard.press( 'Backspace' );
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+		await page.waitForSelector( '.block-editor-block-list__block' );
+		await expect( {
+			selector: '.editor-styles-wrapper .block-editor-block-list__layout.is-root-container > *',
+			property: 'margin-top',
+		} ).cssValueToBe( `15px` );
+		await expect( {
+			selector: '.edit-post-visual-editor pre',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `24px` );
+		await expect( {
+			selector: '.editor-styles-wrapper .block-editor-block-list__layout.is-root-container > *',
+			property: 'margin-left',
+		} ).cssValueToBe( `0px` );
+		await expect( {
+			selector: '.editor-styles-wrapper .block-editor-block-list__layout.is-root-container > *',
+			property: 'margin-right',
+		} ).cssValueToBe( `0px` );
+		await expect( {
+			selector: '.edit-post-visual-editor .block-editor-block-list__block',
+			property: 'padding-right',
+		} ).cssValueToBe( `0px` );
+		await expect( {
+			selector: '.edit-post-visual-editor pre.wp-block',
+			property: 'padding-left',
+		} ).cssValueToBe( `20px` );
+		await expect( {
+			selector: '.edit-post-visual-editor pre',
+			property: 'padding-top',
+		} ).cssValueToBe( `24px` );
+		await expect( {
+			selector: '.edit-post-visual-editor pre',
+			property: 'padding-bottom',
+		} ).cssValueToBe( `24px` );
+	} );
+} );

0bc893235dfd0a9d564713513cffb4ebd71af861
Gaurav Khupse <gauravk@bsf.io>
2021-10-14 09:36:29 +0530

    Removed conflict

--- inc/builder/type/header/woo-cart/dynamic-css/dynamic.css.php
+++ inc/builder/type/header/woo-cart/dynamic-css/dynamic.css.php
@@ -56,8 +56,8 @@ function astra_hb_woo_cart_dynamic_css( $dynamic_css, $dynamic_css_filtered = ''
 	$cart_h_bg_color      = astra_get_option( 'header-woo-cart-background-hover-color' );
 	$cart_separator_color = astra_get_option( 'header-woo-cart-separator-color' );
 	$cart_h_link_color    = astra_get_option( 'header-woo-cart-link-hover-color' );
-
 	$cart_button_text_color   = astra_get_option( 'header-woo-cart-btn-text-color' );
+
 	$cart_button_bg_color     = astra_get_option( 'header-woo-cart-btn-background-color' );
 	$cart_button_text_h_color = astra_get_option( 'header-woo-cart-btn-text-hover-color' );
 	$cart_button_bg_h_color   = astra_get_option( 'header-woo-cart-btn-bg-hover-color' );

24fa7a159ace8d3b5aee5f207a6d8a99f096ac80
Simran <simranc@bsf.io>
2021-10-18 13:56:42 +0530

    added responsive but getting expected and received value difference error in tablet and mobile case

--- tests/e2e/specs/customizer/blog/single-post/postTitleSize.js
+++ tests/e2e/specs/customizer/blog/single-post/postTitleSize.js
@@ -1,16 +1,14 @@
-import {
-	createURL,
-	createNewPost,
-	publishPost,
-} from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
+import { responsiveFontSize } from '../../../../utils/responsive-utils';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 describe( 'post title size in the customizer', () => {
 	it( 'page title size should apply corectly', async () => {
 		const postTitle = {
 			'font-size-entry-title': {
-				desktop: 22,
-				tablet: 20,
-				mobile: 18,
+				desktop: 72,
+				tablet: 42,
+				mobile: 32,
 				'desktop-unit': 'px',
 				'tablet-unit': 'px',
 				'mobile-unit': 'px',
@@ -27,8 +25,31 @@ describe( 'post title size in the customizer', () => {
 		await expect( {
 			selector: '.ast-single-post .entry-title',
 			property: 'font-size',
+		} ).cssValueToBe( `${ postTitle[ 'font-size-entry-title' ].desktop }${ postTitle[ 'font-size-entry-title' ][ 'desktop-unit' ] }`,
+		);
+
+		await setBrowserViewport( 'medium' );
+		await expect( {
+			selector: '.ast-single-post .entry-title',
+			property: 'font-size',
 		} ).cssValueToBe(
-			`${ postTitle[ 'font-size-entry-title' ].desktop }${ postTitle[ 'font-size-entry-title' ][ 'desktop-unit' ] }`,
+			`${ await responsiveFontSize(
+				postTitle[ 'font-size-entry-title' ].tablet,
+			) }${
+				postTitle[ 'font-size-entry-title' ][ 'tablet-unit' ]
+			}`,
+		);
+
+		await setBrowserViewport( 'small' );
+		await expect( {
+			selector: '.ast-single-post .entry-title',
+			property: 'font-size',
+		} ).cssValueToBe(
+			`${ await responsiveFontSize(
+				postTitle[ 'font-size-entry-title' ].mobile,
+			) }${
+				postTitle[ 'font-size-entry-title' ][ 'mobile-unit' ]
+			}`,
 		);
 	} );
 } );

c21a9ff403fdcf6fbd35deacb689cd2af2311212
Simran <simranc@bsf.io>
2021-10-18 16:54:14 +0530

    Added responsive but getting error in expected and received value for tablet and mobile

--- tests/e2e/specs/customizer/Blog-single-post/page-post-title-font-size.test.js
+++ tests/e2e/specs/customizer/Blog-single-post/page-post-title-font-size.test.js
@@ -1,12 +1,14 @@
-import { createURL, createNewPost, publishPost, setBrowserViewport } from '@wordpress/e2e-test-utils';
-import { setCustomize } from '../../../utils/set-customize';
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/customize';
+import { responsiveFontSize } from '../../../utils/responsive-utils';
+import { setBrowserViewport } from '../../../utils/set-browser-viewport';
 describe( 'Post/Page title font size option under the customizer', () => {
 	it( 'post/Page title font size options should apply correctly', async () => {
 		const titlefontsize = {
 			'font-size-entry-title': {
-				desktop: '80',
-				tablet: '50',
-				mobile: '50',
+				desktop: '22',
+				tablet: '20',
+				mobile: '18',
 				'desktop-unit': 'px',
 				'tablet-unit': 'px',
 				'mobile-unit': 'px',
@@ -32,7 +34,6 @@ describe( 'Post/Page title font size option under the customizer', () => {
 		} );
 
 		await page.waitForSelector( ' .ast-single-post .entry-title ' );
-		await setBrowserViewport( 'large' );
 		await expect( {
 			selector: '.ast-single-post .entry-title',
 			property: 'font-size',
@@ -40,17 +41,29 @@ describe( 'Post/Page title font size option under the customizer', () => {
 		);
 
 		await setBrowserViewport( 'medium' );
+
 		await expect( {
 			selector: '.ast-single-post .entry-title',
 			property: 'font-size',
-		} ).cssValueToBe( `${ titlefontsize[ 'font-size-entry-title' ].tablet }${ titlefontsize[ 'font-size-entry-title' ][ 'tablet-unit' ] }`,
+		} ).cssValueToBe(
+			`${ await responsiveFontSize(
+				titlefontsize[ 'font-size-entry-title' ].tablet,
+			) }${
+				titlefontsize[ 'font-size-entry-title' ][ 'tablet-unit' ]
+			}`,
 		);
 
 		await setBrowserViewport( 'small' );
+
 		await expect( {
 			selector: '.ast-single-post .entry-title',
 			property: 'font-size',
-		} ).cssValueToBe( `${ titlefontsize[ 'font-size-entry-title' ].mobile }${ titlefontsize[ 'font-size-entry-title' ][ 'mobile-unit' ] }`,
+		} ).cssValueToBe(
+			`${ await responsiveFontSize(
+				titlefontsize[ 'font-size-entry-title' ].mobile,
+			) }${
+				titlefontsize[ 'font-size-entry-title' ][ 'mobile-unit' ]
+			}`,
 		);
 	} );
 } );

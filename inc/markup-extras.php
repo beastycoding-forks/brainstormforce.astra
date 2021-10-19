12a14a9f9d5064bbffa4a4905da39049ea9cf571
Navanath Bhosale <navanathb@bsf.io>
2021-10-19 15:23:29 +0530

    chore: yoda condition updated

--- inc/markup-extras.php
+++ inc/markup-extras.php
@@ -240,7 +240,7 @@ function astra_get_site_title_tagline( $display_site_title, $display_site_taglin
 		// Site Title.
 		$tag = 'span';
 		if ( is_home() || is_front_page() ) {
-			if ( apply_filters( 'astra_show_site_title_h1_tag', true ) && $device === 'desktop' ) {
+			if ( apply_filters( 'astra_show_site_title_h1_tag', true ) && 'desktop' === $device ) {
 				$tag = 'h1';
 			}
 		}

eb8946e2a07cf05a4d0e4d8d0b248f90977a3a75
Navanath Bhosale <navanathb@bsf.io>
2021-10-20 14:52:15 +0530

    refactor: JS code refactored & cookie dependency is removed now for dark mode

--- inc/core/class-astra-enqueue-scripts.php
+++ inc/core/class-astra-enqueue-scripts.php
@@ -161,7 +161,7 @@ if ( ! class_exists( 'Astra_Enqueue_Scripts' ) ) {
 				}
 
 				if ( Astra_Builder_Helper::is_component_loaded( 'mode-switcher', 'header' ) ) {
-					$default_assets['js']['astra-theme-flyout-cart'] = 'flyout-cart';
+					$default_assets['js']['astra-theme-mode-switcher'] = 'mode-switcher';
 				}
 
 				if ( Astra_Builder_Helper::is_component_loaded( 'edd-cart', 'header' ) ||
@@ -323,6 +323,13 @@ if ( ! class_exists( 'Astra_Enqueue_Scripts' ) ) {
 			);
 
 			wp_localize_script( 'astra-theme-js', 'astra', apply_filters( 'astra_theme_js_localize', $astra_localize ) );
+
+			if ( Astra_Builder_Helper::is_component_loaded( 'mode-switcher', 'header' ) ) {
+				$astra_mode_switcher_localize = array(
+					'carryOsPalette' => astra_get_option( 'mode-switcher-carry-os-palette', false ),
+				);
+				wp_localize_script( 'astra-theme-mode-switcher', 'astraModeSwitcher', apply_filters( 'astra_mode_switcher_localize', $astra_mode_switcher_localize ) );
+			}
 		}
 
 		/**

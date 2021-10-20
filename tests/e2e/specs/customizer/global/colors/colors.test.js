c38410f01b148eaa2ae1cdc6aa4a635c496c7535
Snehal Kolhe <snehalk@bsf.io>
2021-10-20 11:54:06 +0530

    test case for site background color chanded

--- tests/e2e/specs/customizer/global/colors/colors.test.js
+++ tests/e2e/specs/customizer/global/colors/colors.test.js
@@ -9,11 +9,11 @@ describe( 'Testing Global Color setting under the customizer', () => {
 		await setCustomize( textColor );
 		await createNewPost( {
 			postType: 'post',
-			title: 'text-color',
+			title: 'color-test',
 			content: 'this is the text color test',
 		} );
 		await publishPost();
-		await page.goto( createURL( 'text-color' ), {
+		await page.goto( createURL( 'color-test' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( 'body, h1, .entry-title a, .entry-content h1, h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6' );
@@ -27,7 +27,7 @@ describe( 'Testing Global Color setting under the customizer', () => {
 			'heading-base-color': 'rgb(81, 29, 236)',
 		};
 		await setCustomize( headingColor );
-		await page.goto( createURL( 'text-color' ), {
+		await page.goto( createURL( 'color-test' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( '.entry-title' );
@@ -36,34 +36,34 @@ describe( 'Testing Global Color setting under the customizer', () => {
 			property: 'color',
 		} ).cssValueToBe( `${ headingColor[ 'heading-base-color' ] }` );
 	} );
-	// it( 'link color should apply correctly', async () => {
-	// 	const linkColors = {
-	// 		'link-color': 'rgb(64, 24, 211)',
-	// 	};
-	// 	await setCustomize( linkColors );
-	// 	await page.goto( createURL( 'text-color' ), {
-	// 		waitUntil: 'networkidle0',
-	// 	} );
-	// 	await page.waitForSelector( '.entry-meta *' );
-	// 	await expect( {
-	// 		selector: '.entry-meta *',
-	// 		property: 'color',
-	// 	} ).cssValueToBe( `${ linkColors[ 'link-color' ] }` );
-	// } );
-	// it( 'theme color should apply correctly', async () => {
-	// 	const themeColor = {
-	// 		'theme-color': 'rgb(163, 183, 1)',
-	// 	};
-	// 	await setCustomize( themeColor );
-	// 	await page.goto( createURL( 'text-color' ), {
-	// 		waitUntil: 'networkidle0',
-	// 	} );
-	// 	await page.waitForSelector( '.wp-block-search__button' );
-	// 	await expect( {
-	// 		selector: '.wp-block-search__button',
-	// 		property: 'background-color',
-	// 	} ).cssValueToBe( `${ themeColor[ 'theme-color' ] }` );
-	// } );
+	it( 'link color should apply correctly', async () => {
+		const linkColors = {
+			'link-color': 'rgb(64, 24, 211)',
+		};
+		await setCustomize( linkColors );
+		await page.goto( createURL( 'color-test' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.entry-meta *' );
+		await expect( {
+			selector: '.entry-meta *',
+			property: 'color',
+		} ).cssValueToBe( `${ linkColors[ 'link-color' ] }` );
+	} );
+	it( 'theme color should apply correctly', async () => {
+		const themeColor = {
+			'theme-color': 'rgb(163, 183, 1)',
+		};
+		await setCustomize( themeColor );
+		await page.goto( createURL( 'color-test' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.wp-block-search__button' );
+		await expect( {
+			selector: '.wp-block-search__button',
+			property: 'background-color',
+		} ).cssValueToBe( `${ themeColor[ 'theme-color' ] }` );
+	} );
 	it( 'site bg color should apply correctly', async () => {
 		const sitebgColors = {
 			'site-layout-outside-bg-obj-responsive': {
@@ -79,29 +79,29 @@ describe( 'Testing Global Color setting under the customizer', () => {
 			},
 		};
 		await setCustomize( sitebgColors );
-		await page.goto( createURL( 'text-color' ), {
+		await page.goto( createURL( 'color-test' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await setBrowserViewport( 'large' );
+		await page.waitForSelector( '.ast-separate-container' );
 		await expect( {
 			selector: '.ast-separate-container',
 			property: 'background-color',
 		} ).cssValueToBe(
-			`${ sitebgColors[ 'site-layout-outside-bg-obj-responsive' ].desktop }`,
+			`${ sitebgColors[ 'site-layout-outside-bg-obj-responsive' ].desktop[ 'background-color' ] }`,
 		);
 		await setBrowserViewport( 'medium' );
 		await expect( {
 			selector: '.ast-separate-container',
 			property: 'background-color',
 		} ).cssValueToBe(
-			`${ sitebgColors[ 'site-layout-outside-bg-obj-responsive' ].tablet }`,
+			`${ sitebgColors[ 'site-layout-outside-bg-obj-responsive' ].tablet[ 'background-color' ] }`,
 		);
 		await setBrowserViewport( 'small' );
 		await expect( {
 			selector: '.ast-separate-container',
 			property: 'background-color',
 		} ).cssValueToBe(
-			`${ sitebgColors[ 'site-layout-outside-bg-obj-responsive' ].mobile }`,
+			`${ sitebgColors[ 'site-layout-outside-bg-obj-responsive' ].mobile[ 'background-color' ] }`,
 		);
 	} );
 } );

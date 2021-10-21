ab1b97d9e32e130ea2d2e551418277b15111569d
Simran <simranc@bsf.io>
2021-10-21 15:52:53 +0530

    Added responsive and scroll to element in below footer padding test case

--- tests/e2e/specs/customizer/below-footer/margin.test.js
+++ tests/e2e/specs/customizer/below-footer/margin.test.js
@@ -1,5 +1,7 @@
 import { createURL } from '@wordpress/e2e-test-utils';
-import { setCustomize } from '../../../utils/set-customize';
+import { setCustomize } from '../../../utils/customize';
+import { setBrowserViewport } from '../../../utils/set-browser-viewport';
+import { scrollToElement } from '../../../utils/scroll-to-element';
 describe( 'Below footer margin setting in customizer', () => {
 	it( 'margin should apply correctly', async () => {
 		const belowfooterMargin = {
@@ -24,7 +26,7 @@ describe( 'Below footer margin setting in customizer', () => {
 				},
 				'desktop-unit': 'px',
 				'tablet-unit': 'px',
-				'mobile-unit]': 'px',
+				'mobile-unit': 'px',
 			},
 		};
 		await setCustomize( belowfooterMargin );
@@ -37,10 +39,71 @@ describe( 'Below footer margin setting in customizer', () => {
 		} );
 
 		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' );
-
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
-			property: '',
-		} ).cssValueToBe( `` );
+			property: 'margin-top',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].desktop.top }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-right',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].desktop.right }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].desktop.bottom }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-left',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].desktop.left }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'desktop-unit' ] }`,
+		);
+
+		await setBrowserViewport( 'medium' );
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-top',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].tablet.top }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'tablet-unit' ] }`,
+		);
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-right',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].tablet.right }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'tablet-unit' ] }`,
+		);
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].tablet.bottom }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'tablet-unit' ] }`,
+		);
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-left',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].tablet.left }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'tablet-unit' ] }`,
+		);
+
+		await setBrowserViewport( 'small' );
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-top',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].mobile.top }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'mobile-unit' ] }`,
+		);
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-right',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].mobile.right }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'mobile-unit' ] }`,
+		);
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].mobile.bottom }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'mobile-unit' ] }`,
+		);
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'margin-left',
+		} ).cssValueToBe( `${ belowfooterMargin[ 'section-below-footer-builder-margin' ].mobile.left }${ belowfooterMargin[ 'section-below-footer-builder-margin' ][ 'mobile-unit' ] }`,
+		);
 	} );
 } );

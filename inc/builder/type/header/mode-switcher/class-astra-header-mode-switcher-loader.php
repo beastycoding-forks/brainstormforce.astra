30696f6d416aab4bf7411577993fb648e257b646
Navanath Bhosale <navanathb@bsf.io>
2021-11-08 11:14:03 +0530

    fix: loading mode-switcher jerk removed by adding head JS

--- inc/builder/type/header/mode-switcher/class-astra-header-mode-switcher-loader.php
+++ inc/builder/type/header/mode-switcher/class-astra-header-mode-switcher-loader.php
@@ -27,6 +27,7 @@ class Astra_Header_Mode_Switcher_Loader {
 	 */
 	public function __construct() {
 		add_action( 'customize_preview_init', array( $this, 'preview_scripts' ), 110 );
+		add_action( 'wp_head', array( $this, 'mode_preference_script' ) );
 	}
 
 	/**
@@ -42,6 +43,27 @@ class Astra_Header_Mode_Switcher_Loader {
 		$dir_name    = ( true === SCRIPT_DEBUG ) ? 'unminified' : 'minified';
 		$file_prefix = ( true === SCRIPT_DEBUG ) ? '' : '.min';
 		wp_enqueue_script( 'astra-builder-mode-switcher-customizer-preview-js', ASTRA_HEADER_MODE_SWITCHER_URI . '/assets/js/' . $dir_name . '/customizer-preview' . $file_prefix . '.js', array( 'customize-preview', 'astra-customizer-preview-js' ), ASTRA_THEME_VERSION, true );
+	}
+
+	/**
+	 * Adding head frontend script for avoiding jerk while landing on site initially.
+	 *
+	 * @since x.x.x
+	 */
+	public function mode_preference_script() {
+		?>
+			<script type="text/javascript">
+				var siteView = localStorage.getItem( "astra-prefers-color" );
+
+				if ( siteView && siteView === "dark" ) {
+					document.documentElement.classList.add( "ast-dark-site" );
+				}
+
+				if ( siteView && siteView === "light" && document.documentElement.classList.contains( "ast-dark-site" ) ) {
+					document.documentElement.classList.remove( "ast-dark-site" );
+				}
+			</script>
+		<?php
 	}
 }
 

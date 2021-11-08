0a50b7399944734b7924d9dfa7695b599e0dd178
GauravKhupse <gauravk@bsf.io>
2021-11-08 17:02:26 +0530

    Added condition to convert to description

--- inc/customizer/class-astra-customizer.php
+++ inc/customizer/class-astra-customizer.php
@@ -384,7 +384,17 @@ if ( ! class_exists( 'Astra_Customizer' ) ) {
 	                    			return \'&#\'+i.charCodeAt(0)+\';\';
 								});
 	                    	desc.remove();
-	                    	li_wrapper.append(" <i class=\'ast-control-tooltip dashicons dashicons-editor-help\'title=\'" + tooltip +"\'></i>");
+							console.log("---------------------------------------------------------------------");
+							// console.log(desc);
+							console.log(ctrl["id"]);
+							if(ctrl["id"]){
+								li_wrapper.append( tooltip);
+							}else{
+								li_wrapper.append(" <i class=\'ast-control-tooltip dashicons dashicons-editor-help\'title=\'" + tooltip +"\'></i>");
+							}
+
+	                    	
+							
 	                	}
 	            	});
 	        	});';

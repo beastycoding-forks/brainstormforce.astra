3a829c732af998514541511753ef76018dc6b987
Snehal Kolhe <snehalk@bsf.io>
2021-11-10 14:36:44 +0530

    added e2e test case for global typography preset style for preset1

--- tests/e2e/specs/customizer/global/typography/preset1.test.js
+++ tests/e2e/specs/customizer/global/typography/preset1.test.js
@@ -11,33 +11,13 @@ describe( 'Global heading settings in the customizer', () => {
 	it( 'heading font settings should be applied correctly', async () => {
 		const globaltypographyPreset1 = {
 			'body-font-family': "'Open Sans', sans-serif",
-			'body-font-variant': '400',
 			'body-font-weight': '400',
-			'body-text-transform': 'default',
-			'body-line-height': 1.7,
-			'para-margin-bottom': 1.68,
-			'headings-font-family': 'Playfair Display, Georgia, serif',
-			'headings-font-variant': '700',
+			'body-text-transform': 'uppercase',
+			'body-line-height': '25px',
+			'headings-font-family': "'Playfair Display', Georgia, serif",
 			'headings-font-weight': '700',
-			'headings-text-transform': 'Inherit',
-			'headings-line-height': '1.2px',
-			'line-height-h1': '1.2px',
-			'font-family-h1': 'Inherit, Helvetica, Arial, sans-serif',
-			'font-weight-h1': '800',
-			'text-transform-h1': 'uppercase',
-			'line-height-h2': 1.2,
-			'line-height-h3': 1.4,
-			'line-height-h4': 1.2,
-			'line-height-h5': 1.2,
-			'line-height-h6': 1.2,
-			'font-size-body': {
-				desktop: '16',
-				tablet: '16',
-				mobile: '16',
-				'desktop-unit': 'px',
-				'tablet-unit': 'px',
-				'mobile-unit': 'px',
-			},
+			'headings-text-transform': 'capitalize',
+			'headings-line-height': '20px',
 		};
 
 		await setCustomize( globaltypographyPreset1 );
@@ -48,35 +28,43 @@ describe( 'Global heading settings in the customizer', () => {
 		await page.goto( createURL( 'preset1' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.entry-content' );
+		await page.waitForSelector( 'body' );
 
 		await expect( {
-			selector: '.entry-content p',
-			property: 'font-size',
+			selector: 'body',
+			property: 'font-family',
 		} ).cssValueToBe(
-			`${ globaltypographyPreset1[ 'font-size-body' ].desktop }${ globaltypographyPreset1[ 'font-size-body' ][ 'desktop-unit' ] }`,
+			`${ globaltypographyPreset1[ 'body-font-family' ] }`,
 		);
 		await expect( {
-			selector: '.entry-content p',
-			property: 'font-family',
-		} ).cssValueToBe( `${ globaltypographyPreset1[ 'body-font-family' ] }` );
-	} );
-
-	it( 'body typography and heading typography combinations for site title', async () => {
-		const globalTypegraphy = {
-			'body-font-family': "'Open Sans', sans-serif",
-			'body-font-weight': '400',
-			'headings-font-family': 'inherit',
-			'headings-font-weight': '400',
-		};
-
-		await setCustomize( globalTypegraphy );
-		await page.goto( createURL( 'preset1' ), { waitUntil: 'networkidle0' } );
-		await page.waitForSelector( '.entry-content' );
+			selector: 'body',
+			property: 'font-weight',
+		} ).cssValueToBe( `${ globaltypographyPreset1[ 'body-font-weight' ] }` );
+		await expect( {
+			selector: 'body',
+			property: 'text-transform',
+		} ).cssValueToBe( `${ globaltypographyPreset1[ 'body-text-transform' ] }` );
+		await expect( {
+			selector: 'body, button, input, select, textarea, .ast-button, .ast-custom-button',
+			property: 'line-height',
+		} ).cssValueToBe( `${ globaltypographyPreset1[ 'body-line-height' ] }` );
 
 		await expect( {
-			selector: '.site-title a',
+			selector: 'h1, .entry-content h1',
 			property: 'font-family',
-		} ).cssValueToBe( `${ globalTypegraphy[ 'body-font-family' ] }` );
+		} ).cssValueToBe( `${ globaltypographyPreset1[ 'headings-font-family' ] }` );
+		await expect( {
+			selector: '.entry-content h1',
+			property: 'font-weight',
+		} ).cssValueToBe( `${ globaltypographyPreset1[ 'headings-font-weight' ] }` );
+		await expect( {
+			selector: '.entry-content h1',
+			property: 'text-transform',
+		} ).cssValueToBe( `${ globaltypographyPreset1[ 'headings-text-transform' ] }` );
+
+		await expect( {
+			selector: '.entry-content h1',
+			property: 'line-height',
+		} ).cssValueToBe( `${ globaltypographyPreset1[ 'headings-line-height' ] }` );
 	} );
 } );

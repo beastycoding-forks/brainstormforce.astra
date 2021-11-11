71c67131af4e126327dba897bc51b172f7f68db5
Snehal Kolhe <snehalk@bsf.io>
2021-11-11 12:34:07 +0530

    spaces removed

--- tests/e2e/specs/customizer/global/typography/preset1.test.js
+++ tests/e2e/specs/customizer/global/typography/preset1.test.js
@@ -6,7 +6,6 @@ import {
 } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { TPOGRAPHY_TEST_POST_CONTENT } from '../../../../utils/post';
-
 describe( 'Global typography preset 1 settings in the customizer', () => {
 	it( 'body and heading font style for preset 1 should be applied correctly', async () => {
 		const globaltypographyPreset1 = {

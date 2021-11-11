e88d1ce560cf6b3bc2f95060c7db1ea3689ba000
Snehal Kolhe <snehalk@bsf.io>
2021-11-11 13:14:28 +0530

    body and heading font test case separated

--- tests/e2e/specs/customizer/global/typography/preset1.test.js
+++ tests/e2e/specs/customizer/global/typography/preset1.test.js
@@ -7,17 +7,13 @@ import {
 import { setCustomize } from '../../../../utils/customize';
 import { TPOGRAPHY_TEST_POST_CONTENT } from '../../../../utils/post';
 describe( 'Global typography preset 1 settings in the customizer', () => {
-	it( 'body and heading font style for preset 1 should be applied correctly', async () => {
+	it( 'body font style for preset 1 should be applied correctly', async () => {
 		const globaltypographyPreset1 = {
 			'typography-presets': 'Preset1',
 			'body-font-family': "'Open Sans,sans-serif'",
 			'body-font-weight': '400',
 			'body-text-transform': 'uppercase',
 			'body-line-height': '25px',
-			'headings-font-family': "'Playfair Display,Georgia,serif'",
-			'headings-font-weight': '700',
-			'headings-text-transform': 'none',
-			'headings-line-height': '45px',
 		};
 
 		await setCustomize( globaltypographyPreset1 );
@@ -50,25 +46,42 @@ describe( 'Global typography preset 1 settings in the customizer', () => {
 			property: 'line-height',
 		} ).cssValueToBe( `${ globaltypographyPreset1[ 'body-line-height' ] }`,
 		);
+	} );
+	it( 'heading font style for preset 1 should be applied correctly', async () => {
+		const globaltypographyPreset = {
+			'headings-font-family': "'Playfair Display,Georgia,serif'",
+			'headings-font-weight': '700',
+			'headings-text-transform': 'none',
+			'headings-line-height': '45px',
+		};
+		await setCustomize( globaltypographyPreset );
+
+		await createNewPost( { postType: 'post', title: 'preset1' } );
+		await setPostContent( TPOGRAPHY_TEST_POST_CONTENT );
+		await publishPost();
+		await page.goto( createURL( 'preset1' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.entry-content h1' );
 		await expect( {
 			selector: '.entry-content h1',
 			property: 'font-family',
-		} ).cssValueToBe( `${ globaltypographyPreset1[ 'headings-font-family' ] }`,
+		} ).cssValueToBe( `${ globaltypographyPreset[ 'headings-font-family' ] }`,
 		);
 		await expect( {
 			selector: '.entry-content h1',
 			property: 'font-weight',
-		} ).cssValueToBe( `${ globaltypographyPreset1[ 'headings-font-weight' ] }`,
+		} ).cssValueToBe( `${ globaltypographyPreset[ 'headings-font-weight' ] }`,
 		);
 		await expect( {
 			selector: '.entry-content h1',
 			property: 'text-transform',
-		} ).cssValueToBe( `${ globaltypographyPreset1[ 'headings-text-transform' ] }`,
+		} ).cssValueToBe( `${ globaltypographyPreset[ 'headings-text-transform' ] }`,
 		);
 		await expect( {
 			selector: '.entry-content h1',
 			property: 'line-height',
-		} ).cssValueToBe( `${ globaltypographyPreset1[ 'headings-line-height' ] }`,
+		} ).cssValueToBe( `${ globaltypographyPreset[ 'headings-line-height' ] }`,
 		);
 	} );
 } );

9e4bfb9b149ff2c908a1d806d03ceadeba35f898
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-11-18 16:28:33 +0530

    added responsive test case for secondary menu background color

--- tests/e2e/specs/customizer/header-builder/Secondary-menu/add-secondary-menu-background-color-tablet.test.js
+++ tests/e2e/specs/customizer/header-builder/Secondary-menu/add-secondary-menu-background-color-tablet.test.js
@@ -4,19 +4,20 @@ import {
 	publishPost,
 } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
-describe( 'Secondary menu background normal color settings in the customizer for desktop', () => {
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
+describe( 'Secondary menu background normal color settings in the customizer for tablet', () => {
 	//test case for normal color setting
-	it( 'secondary menu background normal color should apply corectly in desktop', async () => {
+	it( 'secondary menu background normal color should apply corectly in tablet', async () => {
 		const secondaryMenuBackgroundColor = {
 			'header-menu2-color-responsive': {
-				desktop: 'rgb(255, 255, 255)',
+				tablet: 'rgb(255, 255, 255)',
 			},
 			'header-menu2-bg-obj-responsive': {
-				desktop: {
+				tablet: {
 					'background-color': 'rgb(167, 1, 118)',
 				},
 			},
-			'header-desktop-items': {
+			'header-mobile-items': {
 				above: {
 					above_left: {
 						0: 'menu-2',
@@ -34,35 +35,37 @@ describe( 'Secondary menu background normal color settings in the customizer for
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '#ast-desktop-header #ast-hf-menu-2' );
+		await setBrowserViewport( 'medium' );
+		await page.waitForSelector( '#ast-mobile-header #ast-hf-menu-2' );
 		await expect( {
 			selector: '#ast-hf-menu-2 .main-header-menu .page_item .menu-link',
 			property: 'color',
 		} ).cssValueToBe(
-			`${ secondaryMenuBackgroundColor[ 'header-menu2-color-responsive' ].desktop }`,
+			`${ secondaryMenuBackgroundColor[ 'header-menu2-color-responsive' ].tablet }`,
 		);
 		await expect( {
 			selector: '.ast-builder-menu-2 .main-header-menu',
 			property: 'background-color',
 		} ).cssValueToBe(
-			`${ secondaryMenuBackgroundColor[ 'header-menu2-bg-obj-responsive' ].desktop[ 'background-color' ] }`,
+			`${ secondaryMenuBackgroundColor[ 'header-menu2-bg-obj-responsive' ].tablet[ 'background-color' ] }`,
 		);
 	} );
 	//test case for active color setting
-	it( 'secondary menu background active color should apply corectly in desktop', async () => {
+	it( 'secondary menu background active color should apply corectly', async () => {
 		const secondaryMenuBackgroundColor = {
 			'header-menu2-a-bg-color-responsive': {
-				desktop: 'rgb(0, 0, 255)',
+				tablet: 'rgb(0, 0, 255)',
 			},
 		};
+		await setBrowserViewport( 'medium' );
 		await setCustomize( secondaryMenuBackgroundColor );
-		await page.click( '.menu-link' );
-		await page.waitForSelector( '#ast-desktop-header #ast-hf-menu-2' );
+		await page.click( '#ast-hf-menu-2 .main-header-menu .menu-item .menu-link' );
+		await page.waitForSelector( '#ast-mobile-header #ast-hf-menu-2' );
 		await expect( {
 			selector: '#secondary_menu-site-navigation #ast-hf-menu-2 .main-header-menu .menu-link',
 			property: 'background-color',
 		} ).cssValueToBe(
-			`${ secondaryMenuBackgroundColor[ 'header-menu2-a-bg-color-responsive' ].desktop }`,
+			`${ secondaryMenuBackgroundColor[ 'header-menu2-a-bg-color-responsive' ].tablet }`,
 		);
 	} );
 } );

1b840db5906b12561fadc2055533b593470f821f
Navanath Bhosale <navanathb@bsf.io>
2021-11-23 10:41:28 +0530

    fix: As switch style is coming from addon, free theme preview triggers preview JS error for that

--- inc/builder/type/footer/mode-switcher/assets/js/unminified/customizer-preview.js
+++ inc/builder/type/footer/mode-switcher/assets/js/unminified/customizer-preview.js
@@ -13,6 +13,7 @@
 	var tablet_break_point    = astraBuilderPreview.tablet_break_point || 768,
 		mobile_break_point    = astraBuilderPreview.mobile_break_point || 544,
 		isProDarkModeActive   = astraFooterModeSwitcherPreview.isDarkModeProActive || false,
+		modeSwitcherStyle     = astraFooterModeSwitcherPreview.modeSwitcherStyle,
 		section = 'footer-section-mode-switcher',
 		selector = '.ast-footer-mode-switcher';
 
@@ -20,8 +21,7 @@
 	wp.customize( 'astra-settings[footer-mode-switcher-icon-size]', function( value ) {
 		value.bind( function( size ) {
 			if( size.desktop != '' || size.tablet != '' || size.mobile != '' ) {
-				var dynamicStyle = '',
-					modeSwitcherStyle = wp.customize( 'astra-settings[footer-dark-mode-switch-style]' ).get() || 'button';
+				var dynamicStyle = '';
 
 				dynamicStyle += selector + ' .ast-mode-switcher-icon {';
 				dynamicStyle += 'height: ' + size.desktop + 'px' + ';';

85a3afb9ff630cf4e59cc12ec00969778b1fb8eb
Snehal Kolhe <snehalk@bsf.io>
2021-11-24 17:17:51 +0530

    added  e2e test case for off canvas background color and close icon color

---
+++ tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/offcanvas-color.test.js
@@ -0,0 +1,40 @@
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../../utils/customize';
+import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
+describe( 'off canvas header background color and close icon color settings in the customizer', () => {
+	it( 'color setting for Off canvas should apply correctly', async () => {
+		const offcanvasColor = {
+			'mobile-header-type': 'full-width',
+			'off-canvas-background': {
+				'background-color': 'rgb(245, 219, 219)',
+			},
+			'off-canvas-close-color': 'rgb(18, 1, 1)',
+		};
+		await setCustomize( offcanvasColor );
+
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'QA',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.ast-mobile-popup-drawer.active .ast-mobile-popup-inner' );
+		await expect( {
+			selector: '.ast-mobile-popup-drawer.active .ast-mobile-popup-inner',
+			property: 'background-color',
+		} ).cssValueToBe( `${ offcanvasColor[ 'off-canvas-background' ][ 'background-color' ] }` );
+		await expect( {
+			selector: '.menu-toggle-close',
+			property: 'color',
+		} ).cssValueToBe( `${ offcanvasColor[ 'off-canvas-close-color' ] }` );
+	} );
+} );

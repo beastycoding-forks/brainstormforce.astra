d4d9bae4c0794d8b41263625b41bac991a5fd196
Snehal Kolhe <snehalk@bsf.io>
2021-11-25 14:51:53 +0530

    rgb color value changed

--- tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/close-icon-color.test.js
+++ tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/close-icon-color.test.js
@@ -5,7 +5,7 @@ describe( 'Off-Canvas header close icon color setting in the customizer', () => 
 	it( 'close icon color for Off-Canvas header should apply correctly', async () => {
 		const closeIconColor = {
 			'mobile-header-type': 'full-width',
-			'off-canvas-close-color': 'rgb(58, 58, 58)',
+			'off-canvas-close-color': 'rgb(255, 26, 26)',
 
 		};
 		await setCustomize( closeIconColor );
@@ -21,9 +21,9 @@ describe( 'Off-Canvas header close icon color setting in the customizer', () => 
 		} );
 		await setBrowserViewport( 'medium' );
 		await page.click( '.main-header-menu-toggle' );
-		await page.waitForSelector( '.ast-mobile-popup-drawer.active .ast-mobile-popup-inner' );
+		await page.waitForSelector( '.ast-mobile-popup-drawer.active .menu-toggle-close' );
 		await expect( {
-			selector: '.ast-mobile-popup-drawer.active .ast-mobile-popup-inner',
+			selector: '.ast-mobile-popup-drawer.active .menu-toggle-close',
 			property: 'color',
 		} ).cssValueToBe( `${ closeIconColor[ 'off-canvas-close-color' ] }` );
 	} );

e200b4959fe57abd13142ae25b2dda46697c8e24
Snehal Kolhe <snehalk@bsf.io>
2021-11-26 15:47:26 +0530

    test case for inner element spacing in the off canvas

---
+++ tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/inner-element-space.test.js
@@ -0,0 +1,41 @@
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../../utils/customize';
+import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
+describe( 'off canvas full-screen header type popup padding setting in the customizer', () => {
+	it( 'padding should apply correctly', async () => {
+		const innerElementSpace = {
+			'mobile-header-type': 'full-width',
+			'header-mobile-items': {
+				popup: {
+					popup_content: {
+						0: 'account',
+						1: 'social-icons-1'					},
+				},
+				'off-canvas-inner-spacing': '40',
+			},
+		};
+		await setCustomize( innerElementSpace );
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'test-page',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.ast-mobile-popup-drawer .ast-mobile-popup-inner' );
+		await expect( {
+			selector: '.ast-mobile-popup-drawer .ast-mobile-popup-inner',
+			property: 'padding-top',
+		} ).cssValueToBe(
+			`${ innerElementSpace[ 'off-canvas-inner-spacing' ] }`,
+		);
+	} );
+} );

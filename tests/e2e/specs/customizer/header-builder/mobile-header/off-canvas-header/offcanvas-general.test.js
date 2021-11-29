a69eef6b30898be817ab828589d2ecbe0bf88abe
Snehal Kolhe <snehalk@bsf.io>
2021-11-29 10:48:51 +0530

    test case for mobile off canvas header general setting

---
+++ tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/offcanvas-general.test.js
@@ -0,0 +1,35 @@
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../../utils/customize';
+import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
+describe( 'off canvas full-screen header type and content alignment settings in the customizer', () => {
+	it( 'off canvas header setting should apply correctly', async () => {
+		const offCanvasHeader = {
+			'mobile-header-type': 'full-width',
+			'header-offcanvas-content-alignment': 'center',
+		};
+		await setCustomize( offCanvasHeader );
+
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'QA',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+
+		await page.waitForSelector( '.ast-builder-menu .main-navigation > ul' );
+		await expect( {
+			selector: '.ast-builder-menu .main-navigation > ul',
+			property: 'align-self',
+		} ).cssValueToBe( `${ offCanvasHeader[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+} );

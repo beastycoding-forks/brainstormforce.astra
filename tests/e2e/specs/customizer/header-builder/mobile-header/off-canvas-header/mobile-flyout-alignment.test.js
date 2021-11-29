043cc997236bb66b2d39a1fe3ef94e6a8cb78422
Simran <simranc@bsf.io>
2021-11-29 14:03:14 +0530

    test case for off canvas header flyout type content alignment for mobile

---
+++ tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/mobile-flyout-alignment.test.js
@@ -0,0 +1,32 @@
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../../utils/customize';
+import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
+describe( 'off canvas flyout header type settings in the customizer', () => {
+	it( 'flyout header alignment center type should apply correctly', async () => {
+		const flyoutAlignment = {
+			'mobile-header-type': 'off-canvas',
+			'header-offcanvas-content-alignment': 'center',
+		};
+		await setCustomize( flyoutAlignment );
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'test-page',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.content-align-center .main-header-menu' );
+		await expect( {
+			selector: '.content-align-center .main-header-menu',
+			property: 'text-align',
+		} ).cssValueToBe( `${ flyoutAlignment[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+} );

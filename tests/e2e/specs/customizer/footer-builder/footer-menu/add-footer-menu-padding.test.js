78bd824b16592c6c351e9dcbd453e8a3e765b2ee
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-12-02 07:24:38 +0530

    added test cases for responsive

--- tests/e2e/specs/customizer/footer-builder/footer-menu/add-footer-menu-padding.test.js
+++ tests/e2e/specs/customizer/footer-builder/footer-menu/add-footer-menu-padding.test.js
@@ -2,8 +2,8 @@ import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils
 import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
-describe( 'Add footer menu spacing for desktop view', () => {
-	it( 'footer menu spacing should be added properly in desktop view', async () => {
+describe( 'Add footer menu spacing', () => {
+	it( 'footer menu spacing should be added properly', async () => {
 		await createNewPost( {
 			postType: 'page',
 			title: 'Test Page',
@@ -60,6 +60,62 @@ describe( 'Add footer menu spacing for desktop view', () => {
 		} );
 		await page.waitForSelector( '.site-footer' );
 		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
+		await expect( {
+			selector: '#astra-footer-menu .menu-item .menu-link',
+			property: 'padding-top',
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '#astra-footer-menu .menu-item .menu-link',
+			property: 'padding-top',
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '#astra-footer-menu .menu-item .menu-link',
+			property: 'padding-left',
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.left }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '#astra-footer-menu .menu-item .menu-link',
+			property: 'padding-right',
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.right }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '#astra-footer-menu .menu-item .menu-link',
+			property: 'padding-bottom',
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.bottom }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		);
+		//for tablet view
+		await setBrowserViewport( 'medium' );
+		await scrollToElement( '#colophon' );
+		await expect( {
+			selector: '#astra-footer-menu .menu-item .menu-link',
+			property: 'padding-top',
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '#astra-footer-menu .menu-item .menu-link',
+			property: 'padding-top',
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '#astra-footer-menu .menu-item .menu-link',
+			property: 'padding-left',
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.left }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '#astra-footer-menu .menu-item .menu-link',
+			property: 'padding-right',
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.right }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '#astra-footer-menu .menu-item .menu-link',
+			property: 'padding-bottom',
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.bottom }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		);
+		//for mobile view
+		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '#astra-footer-menu .menu-item .menu-link',

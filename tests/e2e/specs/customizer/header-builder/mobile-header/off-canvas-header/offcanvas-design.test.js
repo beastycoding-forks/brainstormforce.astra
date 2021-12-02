8259349884fad9b40fe0a17fc80767ff45c07d31
Snehal Kolhe <snehalk@bsf.io>
2021-12-02 17:10:41 +0530

    combined code for tablet and mobile header design

--- tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/offcanvas-design.test.js
+++ tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/offcanvas-design.test.js
@@ -43,7 +43,7 @@ describe( 'Off-Canvas header background color & close icon color setting in the 
 	} );
 
 	//popup padding
-	it( 'padding should apply correctly', async () => {
+	it( 'padding should apply correctly for tablet', async () => {
 		const fullscreenPopupPadding = {
 			'mobile-header-type': 'full-width',
 			'off-canvas-padding': {
@@ -90,6 +90,96 @@ describe( 'Off-Canvas header background color & close icon color setting in the 
 			property: 'padding-left',
 		} ).cssValueToBe(
 			`${ fullscreenPopupPadding[ 'off-canvas-padding' ].tablet.left }${ fullscreenPopupPadding[ 'off-canvas-padding' ][ 'tablet-unit' ] }`,
+		);
+	} );
+	it( 'header design settings for mobile should apply correctly', async () => {
+		const offCanvasColor = {
+			'mobile-header-type': 'full-width',
+			'off-canvas-background': {
+				'background-color': 'rgb(255, 242, 242)',
+			},
+			'off-canvas-close-color': 'rgb(21, 21, 21)',
+		};
+		await setCustomize( offCanvasColor );
+
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+
+		await createNewPost( {
+			postType: 'page',
+			title: 'QA',
+		} );
+		await publishPost();
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.ast-mobile-popup-drawer.active .ast-mobile-popup-inner' );
+		await expect( {
+			selector: '.ast-mobile-popup-drawer.active .ast-mobile-popup-inner',
+			property: 'background-color',
+		} ).cssValueToBe( `${ offCanvasColor[ 'off-canvas-background' ][ 'background-color' ] }` );
+		//close icon color
+		await page.waitForSelector( '.ast-mobile-popup-drawer.active .menu-toggle-close' );
+		await expect( {
+			selector: '.ast-mobile-popup-drawer.active .menu-toggle-close',
+			property: 'color',
+		} ).cssValueToBe( `${ offCanvasColor[ 'off-canvas-close-color' ] }` );
+	} );
+
+	//popup padding
+	it( 'padding should apply correctly for mobile mode', async () => {
+		const fullscreenPopupPadding = {
+			'mobile-header-type': 'full-width',
+			'off-canvas-padding': {
+				mobile: {
+					top: '50',
+					right: '40',
+					bottom: '65',
+					left: '40',
+				},
+				'mobile-unit': 'px',
+
+			},
+		};
+		await setCustomize( fullscreenPopupPadding );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.ast-mobile-popup-drawer.active .ast-mobile-popup-content' );
+		await expect( {
+			selector: '.ast-mobile-popup-drawer.active .ast-mobile-popup-content',
+			property: 'padding-top',
+		} ).cssValueToBe(
+			`${ fullscreenPopupPadding[ 'off-canvas-padding' ].mobile.top }${ fullscreenPopupPadding[ 'off-canvas-padding' ][ 'mobile-unit' ] }`,
+		);
+
+		await expect( {
+			selector: '.ast-mobile-popup-drawer.active .ast-mobile-popup-content',
+			property: 'padding-right',
+		} ).cssValueToBe(
+			`${ fullscreenPopupPadding[ 'off-canvas-padding' ].mobile.right }${ fullscreenPopupPadding[ 'off-canvas-padding' ][ 'mobile-unit' ] }`,
+		);
+
+		await expect( {
+			selector: '.ast-mobile-popup-drawer.active .ast-mobile-popup-content',
+			property: 'padding-bottom',
+		} ).cssValueToBe(
+			`${ fullscreenPopupPadding[ 'off-canvas-padding' ].mobile.bottom }${ fullscreenPopupPadding[ 'off-canvas-padding' ][ 'mobile-unit' ] }`,
+		);
+
+		await expect( {
+			selector: '.ast-mobile-popup-drawer.active .ast-mobile-popup-content',
+			property: 'padding-left',
+		} ).cssValueToBe(
+			`${ fullscreenPopupPadding[ 'off-canvas-padding' ].mobile.left }${ fullscreenPopupPadding[ 'off-canvas-padding' ][ 'mobile-unit' ] }`,
 		);
 	} );
 } );

6014af69db0458e2af85d3759ebb3fecf792cd01
premanshup <premanshup@bsf.io>
2021-12-03 20:34:10 +0530

    Remove usused config params.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
 	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
-	ignorePatterns: [ 'deprecated.js' ],
-	globals: {
-		automatePlugData: true,
-		jQuery: true,
-	},
 	parser: '@babel/eslint-parser',
 	parserOptions: {
 		requireConfigFile: false,

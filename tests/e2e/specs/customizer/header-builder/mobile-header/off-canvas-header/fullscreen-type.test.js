606c26272bb18370eda659992e09b317363f43c7
Snehal Kolhe <snehalk@bsf.io>
2021-12-06 16:53:31 +0530

    added esponsive test cases for alignment

--- tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/fullscreen-type.test.js
+++ tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/fullscreen-type.test.js
@@ -11,7 +11,7 @@ describe( 'off canvas full-screen header type and content alignment settings for
 
 		await createNewPost( {
 			postType: 'page',
-			title: 'sample-page',
+			title: 'align-left',
 		} );
 		await publishPost();
 		await page.goto( createURL( '/' ), {
@@ -26,8 +26,54 @@ describe( 'off canvas full-screen header type and content alignment settings for
 			selector: '.content-align-flex-start .ast-builder-layout-element',
 			property: 'justify-content',
 		} ).cssValueToBe( `${ offCanvasGeneralSetting[ 'header-offcanvas-content-alignment' ] }` );
+		
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+
+		await page.waitForSelector( '.content-align-flex-start .ast-builder-layout-element' );
+		await expect( {
+			selector: '.content-align-flex-start .ast-builder-layout-element',
+			property: 'justify-content',
+		} ).cssValueToBe( `${ offCanvasGeneralSetting[ 'header-offcanvas-content-alignment' ] }` );
+
 	} );
-	it( 'off canvas header general setting should apply correctly for mobile mode', async () => {
+
+	it( 'off canvas header content alignment as center should apply correctly for mobile mode', async () => {
+		const offCanvasGeneralSetting = {
+			'mobile-header-type': 'full-width',
+			'header-offcanvas-content-alignment': 'center',
+		};
+		await setCustomize( offCanvasGeneralSetting );
+
+		await createNewPost( {
+			postType: 'page',
+			title: 'align-center',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+
+		await page.waitForSelector( '.content-align-center .ast-builder-layout-element' );
+		await expect( {
+			selector: '.content-align-center .ast-builder-layout-element',
+			property: 'justify-content',
+		} ).cssValueToBe( `${ offCanvasGeneralSetting[ 'header-offcanvas-content-alignment' ] }` );
+
+
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+
+		await page.waitForSelector( '.content-align-center .ast-builder-layout-element' );
+		await expect( {
+			selector: '.content-align-center .ast-builder-layout-element',
+			property: 'justify-content',
+		} ).cssValueToBe( `${ offCanvasGeneralSetting[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+	it( 'off canvas header content alignment as Right apply correctly for mobile mode', async () => {
 		const offCanvasGeneralSetting = {
 			'mobile-header-type': 'full-width',
 			'header-offcanvas-content-alignment': 'flex-end',
@@ -36,12 +82,22 @@ describe( 'off canvas full-screen header type and content alignment settings for
 
 		await createNewPost( {
 			postType: 'page',
-			title: 'test-page',
+			title: 'align-right',
 		} );
 		await publishPost();
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
+
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+
+		await page.waitForSelector( '.content-align-flex-end .ast-builder-layout-element' );
+		await expect( {
+			selector: '.content-align-flex-end .ast-builder-layout-element',
+			property: 'justify-content',
+		} ).cssValueToBe( `${ offCanvasGeneralSetting[ 'header-offcanvas-content-alignment' ] }` );
+
 
 		await setBrowserViewport( 'small' );
 		await page.click( '.main-header-menu-toggle' );

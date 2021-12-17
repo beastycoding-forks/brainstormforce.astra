7358b9772f595027ac3adf16534893bea237cba4
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-12-17 13:06:06 +0530

    added util for create menu in the test case

--- tests/e2e/specs/customizer/footer-builder/footer-menu/alignment.test.js
+++ tests/e2e/specs/customizer/footer-builder/footer-menu/alignment.test.js
@@ -2,40 +2,10 @@ import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils
 import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
+import { createNewMenu } from '../../../../utils/create-menu'
 describe( 'Add footer menu spacing', () => {
 	it( 'footer menu center alignment should be added properly', async () => {
-		await createNewPost( {
-			postType: 'page',
-			title: 'Test Page',
-			content: 'This is simple test page',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'page',
-			title: 'Sub Test Page',
-			content: 'This is simple sub test page',
-		} );
-		await publishPost();
-		await page.goto( createURL( '/wp-admin/nav-menus.php' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await scrollToElement( '#nav-menu-footer' );
-		if ( await page.$( '.menu-delete' ) ) {
-			await page.click( '.menu-delete' );
-		}
-		await page.focus( '#menu-name' );
-		await page.type( '#menu-name', 'Menu' );
-		await page.focus( '#locations-footer_menu' );
-		await page.click( '#locations-footer_menu' );
-		await page.click( '#save_menu_footer' );
-		await page.waitForSelector( '.accordion-section-content ' );
-		await page.focus( '#page-tab' );
-		await page.click( '#page-tab' );
-		await page.click( '#submit-posttype-page' );
-		await scrollToElement( '#nav-menu-footer' );
-		await page.waitForSelector( '.publishing-action' );
-		await page.focus( '#save_menu_footer' );
-		await page.click( '#save_menu_footer' );
+		await createNewMenu();
 		//center alignment for desktop, tablet and mobile
 		const footerMenuAlignment = {
 			'footer-menu-alignment': {

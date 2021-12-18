b4b772df5f7bfed06847692ac0106be60037401c
Simran <simranc@bsf.io>
2021-12-18 16:54:28 +0530

    modified code and resolved error

--- tests/e2e/specs/customizer/footer-builder/footer-menu/add-footer-menu-padding.test.js
+++ tests/e2e/specs/customizer/footer-builder/footer-menu/add-footer-menu-padding.test.js
@@ -4,38 +4,6 @@ import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
 describe( 'Add footer menu spacing', () => {
 	it( 'footer menu spacing should be added properly', async () => {
-		await createNewPost( {
-			postType: 'page',
-			title: 'Test Page',
-			content: 'This is simple test page',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'page',
-			title: 'Sub Test Page',
-			content: 'This is simple sub test page',
-		} );
-		await publishPost();
-		await page.goto( createURL( '/wp-admin/nav-menus.php' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await scrollToElement( '#nav-menu-footer' );
-		if ( await page.$( '.menu-delete' ) ) {
-			await page.click( '.menu-delete' );
-		}
-		await page.focus( '#menu-name' );
-		await page.type( '#menu-name', 'Menu' );
-		await page.focus( '#locations-footer_menu' );
-		await page.click( '#locations-footer_menu' );
-		await page.click( '#save_menu_footer' );
-		await page.waitForSelector( '.accordion-section-content ' );
-		await page.focus( '#page-tab' );
-		await page.click( '#page-tab' );
-		await page.click( '#submit-posttype-page' );
-		await scrollToElement( '#nav-menu-footer' );
-		await page.waitForSelector( '.publishing-action' );
-		await page.focus( '#save_menu_footer' );
-		await page.click( '#save_menu_footer' );
 		const footerMenuSpacing = {
 			'footer-main-menu-spacing': {
 				desktop: {
@@ -54,93 +22,141 @@ describe( 'Add footer menu spacing', () => {
 				},
 			},
 		};
+		await createNewPost( {
+			postType: 'page',
+			title: 'footer Page',
+		} );
+		await publishPost();
 		await setCustomize( footerMenuSpacing );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-footer' );
+		await page.waitForSelector( '#astra-footer-menu .menu-item > a' );
 		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-top',
 		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
 		);
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
-			property: 'padding-top',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
-		);
-		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-left',
 		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.left }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
 		);
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-right',
 		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.right }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
 		);
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-bottom',
 		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.bottom }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
 		);
-		//for tablet view
+	} );
+	it( 'footer menu spacing for tablet should be added properly', async () => {
+		const footerMenuSpacing = {
+			'footer-main-menu-spacing': {
+				tablet: {
+					top: '100',
+					right: '100',
+					bottom: '100',
+					left: '100',
+				},
+				'tablet-unit': 'px',
+			},
+			'footer-desktop-items': {
+				above: {
+					above_1: {
+						0: 'menu',
+					},
+				},
+			},
+		};
+		await createNewPost( {
+			postType: 'page',
+			title: 'footer Page',
+		} );
+		await publishPost();
+		await setCustomize( footerMenuSpacing );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '#astra-footer-menu .menu-item > a' );
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-top',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].tablet.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'tablet-unit' ] }`,
 		);
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
-			property: 'padding-top',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
-		);
-		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-left',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.left }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].tablet.left }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'tablet-unit' ] }`,
 		);
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-right',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.right }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].tablet.right }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'tablet-unit' ] }`,
 		);
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-bottom',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.bottom }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].tablet.bottom }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'tablet-unit' ] }`,
 		);
-		//for mobile view
+	} );
+	it( 'footer menu spacing for mobile should be added properly', async () => {
+		const footerMenuSpacing = {
+			'footer-main-menu-spacing': {
+				mobile: {
+					top: '100',
+					right: '100',
+					bottom: '100',
+					left: '100',
+				},
+				'mobile-unit': 'px',
+			},
+			'footer-desktop-items': {
+				above: {
+					above_1: {
+						0: 'menu',
+					},
+				},
+			},
+		};
+		await createNewPost( {
+			postType: 'page',
+			title: 'footer Page',
+		} );
+		await publishPost();
+		await setCustomize( footerMenuSpacing );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '#astra-footer-menu .menu-item > a' );
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-top',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].mobile.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'mobile-unit' ] }`,
 		);
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
-			property: 'padding-top',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.top }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
-		);
-		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-left',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.left }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].mobile.left }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'mobile-unit' ] }`,
 		);
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-right',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.right }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].mobile.right }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'mobile-unit' ] }`,
 		);
 		await expect( {
-			selector: '#astra-footer-menu .menu-item .menu-link',
+			selector: '#astra-footer-menu .menu-item > a',
 			property: 'padding-bottom',
-		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].desktop.bottom }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ footerMenuSpacing[ 'footer-main-menu-spacing' ].mobile.bottom }${ footerMenuSpacing[ 'footer-main-menu-spacing' ][ 'mobile-unit' ] }`,
 		);
 	} );
 } );

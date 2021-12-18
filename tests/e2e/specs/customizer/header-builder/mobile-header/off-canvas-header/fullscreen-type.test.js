5ad06669de834a82184e277735b088187994f885
Simran <simranc@bsf.io>
2021-12-18 07:00:08 +0530

    modified code

--- tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/fullscreen-type.test.js
+++ tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/fullscreen-type.test.js
@@ -2,7 +2,7 @@ import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils
 import { setCustomize } from '../../../../../utils/customize';
 import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
 describe( 'off canvas header type and content alignment settings in the customizer', () => {
-	it( 'off canvas header general setting should apply correctly', async () => {
+	it( 'off canvas header align left for tablet should apply correctly', async () => {
 		const offCanvasGeneralSetting = {
 			'mobile-header-type': 'full-width',
 			'header-offcanvas-content-alignment': 'flex-start',
@@ -26,6 +26,22 @@ describe( 'off canvas header type and content alignment settings in the customiz
 			selector: '.content-align-flex-start .ast-builder-layout-element',
 			property: 'justify-content',
 		} ).cssValueToBe( `${ offCanvasGeneralSetting[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+	it( 'off canvas header align left for mobile should apply correctly', async () => {
+		const offCanvasGeneralSetting = {
+			'mobile-header-type': 'full-width',
+			'header-offcanvas-content-alignment': 'flex-start',
+		};
+		await setCustomize( offCanvasGeneralSetting );
+
+		await createNewPost( {
+			postType: 'page',
+			title: 'align-left',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
 
 		await setBrowserViewport( 'small' );
 		await page.click( '.main-header-menu-toggle' );
@@ -37,7 +53,7 @@ describe( 'off canvas header type and content alignment settings in the customiz
 		} ).cssValueToBe( `${ offCanvasGeneralSetting[ 'header-offcanvas-content-alignment' ] }` );
 	} );
 
-	it( 'off canvas header content alignment as center should apply correctly for mobile mode', async () => {
+	it( 'off canvas header align center for tablet should apply correctly for mobile mode', async () => {
 		const offCanvasGeneralSetting = {
 			'mobile-header-type': 'full-width',
 			'header-offcanvas-content-alignment': 'center',
@@ -61,7 +77,22 @@ describe( 'off canvas header type and content alignment settings in the customiz
 			selector: '.content-align-center .ast-builder-layout-element',
 			property: 'justify-content',
 		} ).cssValueToBe( `${ offCanvasGeneralSetting[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+	it( 'off canvas header align center for mobile should apply correctly for mobile mode', async () => {
+		const offCanvasGeneralSetting = {
+			'mobile-header-type': 'full-width',
+			'header-offcanvas-content-alignment': 'center',
+		};
+		await setCustomize( offCanvasGeneralSetting );
 
+		await createNewPost( {
+			postType: 'page',
+			title: 'align-center',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
 		await setBrowserViewport( 'small' );
 		await page.click( '.main-header-menu-toggle' );
 		await page.waitForSelector( '.content-align-center .ast-builder-layout-element' );
@@ -70,7 +101,7 @@ describe( 'off canvas header type and content alignment settings in the customiz
 			property: 'justify-content',
 		} ).cssValueToBe( `${ offCanvasGeneralSetting[ 'header-offcanvas-content-alignment' ] }` );
 	} );
-	it( 'off canvas header content alignment as Right apply correctly for mobile mode', async () => {
+	it( 'off canvas header align Right for tablet apply correctly for mobile mode', async () => {
 		const offCanvasGeneralSetting = {
 			'mobile-header-type': 'full-width',
 			'header-offcanvas-content-alignment': 'flex-end',
@@ -94,6 +125,22 @@ describe( 'off canvas header type and content alignment settings in the customiz
 			selector: '.content-align-flex-end .ast-builder-layout-element',
 			property: 'justify-content',
 		} ).cssValueToBe( `${ offCanvasGeneralSetting[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+	it( 'off canvas header align Right for mobile apply correctly for mobile mode', async () => {
+		const offCanvasGeneralSetting = {
+			'mobile-header-type': 'full-width',
+			'header-offcanvas-content-alignment': 'flex-end',
+		};
+		await setCustomize( offCanvasGeneralSetting );
+
+		await createNewPost( {
+			postType: 'page',
+			title: 'align-right',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
 
 		await setBrowserViewport( 'small' );
 		await page.click( '.main-header-menu-toggle' );

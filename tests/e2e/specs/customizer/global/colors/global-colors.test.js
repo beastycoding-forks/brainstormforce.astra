f6b246cd3cad7cb6ea36cc1e6376775aa078b1ab
Navanath Bhosale <navanathb@bsf.io>
2021-12-20 09:17:46 +0530

    chore: Pulled latest code & moved color option specific file in colors folder

--- tests/e2e/specs/customizer/global/colors/global-colors.test.js
+++ tests/e2e/specs/customizer/global/colors/global-colors.test.js
@@ -1,6 +1,7 @@
 import { setCustomize } from '../../../../utils/customize';
 import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
 describe( 'Testing Global Color setting under the customizer', () => {
+
 	it( 'text color should apply correctly', async () => {
 		const textandheadingColor = {
 			'text-color': 'rgb(205, 41, 41)',
@@ -9,11 +10,11 @@ describe( 'Testing Global Color setting under the customizer', () => {
 		await setCustomize( textandheadingColor );
 		await createNewPost( {
 			postType: 'post',
-			title: 'color-test',
+			title: 'Global Colors Test',
 			content: 'this is the text color test',
 		} );
 		await publishPost();
-		await page.goto( createURL( 'color-test' ), {
+		await page.goto( createURL( 'global-colors-test' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( 'body, h1, .entry-title a, .entry-content h1, h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6' );
@@ -28,12 +29,13 @@ describe( 'Testing Global Color setting under the customizer', () => {
 			property: 'color',
 		} ).cssValueToBe( `${ textandheadingColor[ 'heading-base-color' ] }` );
 	} );
+
 	it( 'link color should apply correctly', async () => {
 		const linkColors = {
 			'link-color': 'rgb(64, 24, 211)',
 		};
 		await setCustomize( linkColors );
-		await page.goto( createURL( 'color-test' ), {
+		await page.goto( createURL( 'global-colors-test' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( '.entry-meta *' );
@@ -42,12 +44,13 @@ describe( 'Testing Global Color setting under the customizer', () => {
 			property: 'color',
 		} ).cssValueToBe( `${ linkColors[ 'link-color' ] }` );
 	} );
+
 	it( 'theme color should apply correctly', async () => {
 		const themeColor = {
 			'theme-color': 'rgb(163, 183, 1)',
 		};
 		await setCustomize( themeColor );
-		await page.goto( createURL( 'color-test' ), {
+		await page.goto( createURL( 'global-colors-test' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( '.wp-block-search__button' );
@@ -56,22 +59,4 @@ describe( 'Testing Global Color setting under the customizer', () => {
 			property: 'background-color',
 		} ).cssValueToBe( `${ themeColor[ 'theme-color' ] }` );
 	} );
-
-	// GitHub action E2E fail case
-	// eslint-disable-next-line jest/no-commented-out-tests
-	// it( 'link hover color should apply correctly', async () => {
-	// 	const linkhoverColor = {
-	// 		'link-h-color': 'rgb(205, 41, 41)',
-	// 	};
-	// 	await setCustomize( linkhoverColor );
-	// 	await page.goto( createURL( 'color-test' ), {
-	// 		waitUntil: 'networkidle0',
-	// 	} );
-	// 	await page.hover( '.wp-block-group__inner-container :last-child' );
-	// 	await page.waitForSelector( '.entry-title' );
-	// 	await expect( {
-	// 		selector: '.wp-block-group__inner-container :last-child',
-	// 		property: 'color',
-	// 	} ).cssValueToBe( `${ linkhoverColor[ 'link-h-color' ] }` );
-	// } );
 } );

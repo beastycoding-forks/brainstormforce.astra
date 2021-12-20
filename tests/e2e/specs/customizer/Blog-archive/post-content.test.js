4468ee0a5444ddeca0af6b90b73b4231b3d4e19a
Simran <simranc@bsf.io>
2021-12-20 18:44:35 +0530

    modified code

--- tests/e2e/specs/customizer/Blog-archive/post-content.test.js
+++ tests/e2e/specs/customizer/Blog-archive/post-content.test.js
@@ -10,41 +10,35 @@ describe( 'Blog Archive post content option under the customizer', () => {
 		await createNewPost( {
 			postType: 'post',
 			title: 'blog-post',
-			content: 'Full Content is applying properly',
-			excerpt: 'Excerpt is applying properly',
+			content: 'Before & After magazine answered a curious reader Its words loosely approximate the frequency with which letters occur in English which is why at a glance it looks pretty real Until recently the prevailing view assumed lorem ipsum was born as a nonsense text It s not Latin though it looks like it and it actually says nothing Before & After magazine answered a curious reade Its words loosely approximate the frequency',
 		} );
 		await publishPost();
 
 		await page.goto( createURL( '/author/admin' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.ast-separate-container .ast-article-post  ' );
-		await expect( {
-			selector: '.ast-separate-container .ast-article-post  ',
-			property: '',
-		} ).cssValueToBe( `` );
+		await page.waitForSelector( '.read-more a' );
+		const excerpt = await page.$eval( '.read-more a', ( element ) => element.getAttribute( 'href' ) );
+		await expect( excerpt ).toBe( 'http://localhost:8888/blog-post' );
 	} );
 
-	it( 'blog Archive full content options should apply correctly', async () => {
-		const postcontent = {
-			'blog-post-content': 'full-content',
-		};
-		await setCustomize( postcontent );
-		await createNewPost( {
-			postType: 'post',
-			title: 'blog-post',
-			content: 'Full Content is applying properly',
-			excerpt: 'Excerpt is applying properly',
-		} );
-		await publishPost();
+	// it( 'blog Archive full content options should apply correctly', async () => {
+	// 	const postcontent = {
+	// 		'blog-post-content': 'full-content',
+	// 	};
+	// 	await setCustomize( postcontent );
+	// 	await createNewPost( {
+	// 		postType: 'post',
+	// 		title: 'blog-post',
+	// 		content: 'Before & After magazine answered a curious reader Its words loosely approximate the frequency with which letters occur in English which is why at a glance it looks pretty real Until recently the prevailing view assumed lorem ipsum was born as a nonsense text It s not Latin though it looks like it and it actually says nothing Before & After magazine answered a curious reade Its words loosely approximate the frequency',
+	// 	} );
+	// 	await publishPost();
 
-		await page.goto( createURL( '/author/admin' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await page.waitForSelector( '.ast-separate-container .ast-article-post  ' );
-		await expect( {
-			selector: '.ast-separate-container .ast-article-post  ',
-			property: '',
-		} ).cssValueToBe( `` );
-	} );
+	// 	await page.goto( createURL( '/author/admin' ), {
+	// 		waitUntil: 'networkidle0',
+	// 	} );
+	// 	await page.waitForSelector( '.entry-content' );
+	// 	const fullContent = await page.$eval( '.entry-content', ( element ) => element.getAttribute( 'href' ) );
+	// 	await expect( fullContent ).toBe( NULL );
+	// } );
 } );

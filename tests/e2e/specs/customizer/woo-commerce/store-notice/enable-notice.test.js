30fcdc1126ce3aa51507b8ccbeda23b4e1c468ff
Snehal Kolhe <snehalk@bsf.io>
2021-12-21 10:59:48 +0530

    asserion tried

--- tests/e2e/specs/customizer/woo-commerce/store-notice/enable-notice.test.js
+++ tests/e2e/specs/customizer/woo-commerce/store-notice/enable-notice.test.js
@@ -1,5 +1,5 @@
 import {
-	createURL,
+	createURL, createNewPost, publishPost,
 } from '@wordpress/e2e-test-utils';
 
 import { setCustomize } from '../../../../utils/customize';
@@ -7,17 +7,20 @@ import { setCustomize } from '../../../../utils/customize';
 describe( 'Global typography H1 settings in the customizer', () => {
 	it( 'heading 1 font family settings should be applied correctly', async () => {
 		const enableNotice = {
-			'_customize-input-woocommerce_demo_store': 'yes',
+			woocommerce_demo_store: 'yes',
 
 		};
 		await setCustomize( enableNotice );
-		await page.goto( createURL( '/' ), {
+		await createNewPost( { postType: 'page', title: 'storenotice' } );
+		await publishPost();
+		await page.goto( createURL( 'storenotice' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( 'p.demo_store, .woocommerce-store-notice' );
+
 		await expect( {
-			selector: 'p.demo_store, .woocommerce-store-notice h1',
-			property: '',
-		} ).cssValueToBe( `${ enableNotice.woocommerce_demo_store }` );
+			selector: 'p.demo_store, .woocommerce-store-notice',
+			property: 'position',
+		} ).cssValueToBe( `${ 'woocommerce_demo_store' }` );
 	} );
 } );

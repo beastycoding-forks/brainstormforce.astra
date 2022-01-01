9af7618d3f7c959584275ae6b29a845641083d1f
Simran <simranc@bsf.io>
2022-01-01 15:18:53 +0530

    added test case for related post by option

--- tests/e2e/specs/customizer/blog/single-post/post-query.test.js
+++ tests/e2e/specs/customizer/blog/single-post/post-query.test.js
@@ -1,10 +1,10 @@
 import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Single post option under the customizer', () => {
-	it( 'post query Order by option should apply correctly', async () => {
+	it( 'post query Related post by by option should apply correctly', async () => {
 		const GridandColumns = {
 			'enable-related-posts': true,
-			'related-posts-order': 'desc',
+			'related-posts-based-on': 'categories',
 		};
 		await setCustomize( GridandColumns );
 		await createNewPost( {
@@ -29,8 +29,39 @@ describe( 'Single post option under the customizer', () => {
 			window.scrollBy( 0, window.innerHeight );
 		} );
 		await page.waitForSelector( '.ast-related-posts-wrapper' );
-		const Order = await page.$eval( '.ast-related-posts-wrapper', ( element ) => element.getAttribute( '.ast-single-related-posts-container .ast-related-posts-wrapper' ) );
-		await expect( Order ).toBeNull( );
+		const Relatedpostby = await page.$eval( '.ast-related-posts-wrapper', ( element ) => element.getAttribute( '.ast-single-related-posts-container .ast-related-posts-wrapper' ) );
+		await expect( Relatedpostby ).toBeNull( );
+	} );
+	it( 'post query Order by option should apply correctly', async () => {
+		const GridandColumns = {
+			'enable-related-posts': true,
+			'related-posts-order-by': 'title',
+		};
+		await setCustomize( GridandColumns );
+		await createNewPost( {
+			postType: 'post',
+			title: 'a-post',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'post',
+			title: 'b-post',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'post',
+			title: 'c-post',
+		} );
+		await publishPost();
+		await page.goto( createURL( 'a-post' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.evaluate( () => {
+			window.scrollBy( 0, window.innerHeight );
+		} );
+		await page.waitForSelector( '.ast-related-posts-wrapper' );
+		const Orderby = await page.$eval( '.ast-related-posts-wrapper', ( element ) => element.getAttribute( '.ast-single-related-posts-container .ast-related-posts-wrapper' ) );
+		await expect( Orderby ).toBeNull( );
 	} );
 	it( 'post query Order option should apply correctly', async () => {
 		const GridandColumns = {

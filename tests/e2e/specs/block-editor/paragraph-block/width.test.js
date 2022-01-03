11d112c398d6a6ab6bbfecbf78f2f4fd642e4c51
Snehal Kolhe <snehalk@bsf.io>
2022-01-03 17:20:57 +0530

    selectors changed

--- tests/e2e/specs/block-editor/paragraph-block/width.test.js
+++ tests/e2e/specs/block-editor/paragraph-block/width.test.js
@@ -13,36 +13,36 @@ describe( 'Paragraph in gutenberg editor', () => {
 		await insertBlock( 'Paragraph' );
 		await page.keyboard.type( 'paragraph' );
 
-		await page.waitForSelector( '#editor .edit-post-visual-editor' );
+		await page.waitForSelector( '.block-editor-block-list__layout' );
 		await expect( {
-			selector: '.edit-post-visual-editor p',
+			selector: '.block-editor-block-list__layout',
 			property: 'width',
 		} ).cssValueToBe( `910px` );
 	} );
 	it( 'assert margin property of the paragraph in the block editor', async () => {
-		await page.waitForSelector( '#editor .edit-post-visual-editor' );
+		await page.waitForSelector( '.block-editor-block-list__layout' );
 		await expect( {
 			selector: '.edit-post-visual-editor p',
 			property: 'margin-top',
 		} ).cssValueToBe( `28px` );
-		await page.waitForSelector( '#editor .edit-post-visual-editor' );
+		await page.waitForSelector( '.block-editor-block-list__layout' );
 		await expect( {
 			selector: '.edit-post-visual-editor p',
 			property: 'margin-right',
 		} ).cssValueToBe( `104.5px` );
-		await page.waitForSelector( '#editor .edit-post-visual-editor' );
+		await page.waitForSelector( '.block-editor-block-list__layout' );
 		await expect( {
 			selector: '.edit-post-visual-editor p',
 			property: 'margin-left',
 		} ).cssValueToBe( `0px` );
-		await page.waitForSelector( '#editor .edit-post-visual-editor' );
+		await page.waitForSelector( '.block-editor-block-list__layout' );
 		await expect( {
 			selector: '.edit-post-visual-editor p',
 			property: 'margin-bottom',
 		} ).cssValueToBe( `28px` );
 	} );
 	it( 'assert padding property of the paragraph in the block editor', async () => {
-		await page.waitForSelector( '#editor .edit-post-visual-editor' );
+		await page.waitForSelector( '.block-editor-block-list__layout' );
 		await expect( {
 			selector: '.edit-post-visual-editor p',
 			property: 'padding',

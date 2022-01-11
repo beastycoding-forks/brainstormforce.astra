3a44057d9f82aa48129ceeb719b3ecd7db3d961c
Simran <simranc@bsf.io>
2022-01-11 16:24:15 +0530

    modified code

--- tests/e2e/specs/block-editor/audio-block/audio-block-width.test.js
+++ tests/e2e/specs/block-editor/audio-block/audio-block-width.test.js
@@ -1,6 +1,6 @@
 import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
-describe( 'List in gutenberg editor', () => {
-	it( 'assert wide width of the list in the block editor', async () => {
+describe( 'Audio block in gutenberg editor', () => {
+	it( 'assert width of the audio in the block editor', async () => {
 		await createNewPost( { postType: 'post', title: 'test audio block' } );
 		await insertBlock( 'Audio' );
 		await page.waitForSelector( '.components-placeholder.components-placeholder' );

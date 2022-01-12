d0ee8c688bb147d95cdc90381b0cd621bcdba324
Simran <simranc@bsf.io>
2022-01-12 16:40:13 +0530

    modified code

--- tests/e2e/specs/block-editor/pullquote-block/width.test.js
+++ tests/e2e/specs/block-editor/pullquote-block/width.test.js
@@ -1,6 +1,6 @@
 import { insertBlock, createNewPost, clickBlockToolbarButton } from '@wordpress/e2e-test-utils';
 describe( 'Pullquote in gutenberg editor', () => {
-	it( 'assert wide and full width of the pullquote in the block editor', async () => {
+	it( 'assert wide, full and default width of the pullquote in the block editor', async () => {
 		await createNewPost( { postType: 'post', title: 'test Pullquote' } );
 		await page.click( '[aria-label="Settings"]' );
 		await insertBlock( 'Pullquote' );

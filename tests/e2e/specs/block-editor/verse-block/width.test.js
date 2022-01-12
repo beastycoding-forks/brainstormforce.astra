f8015c3ae1b7d77d92934c24112b1f72c8a4943c
Simran <simranc@bsf.io>
2022-01-12 15:22:34 +0530

    test case for default width verse

--- tests/e2e/specs/block-editor/verse-block/width.test.js
+++ tests/e2e/specs/block-editor/verse-block/width.test.js
@@ -1,60 +1,13 @@
 import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
 describe( 'verse in gutenberg editor', () => {
-	it( 'assert wide width of the verse in the block editor', async () => {
-		await createNewPost( {
-			postType: 'post',
-			title: 'test verse',
-		} );
+	it( 'assert default width of the verse in the block editor', async () => {
+		await createNewPost( { postType: 'post', title: 'test verse' } );
 		await insertBlock( 'Verse' );
+		//default width for verse block
 		await page.waitForSelector( '.wp-block-verse' );
 		await expect( {
 			selector: '.wp-block-verse',
 			property: 'width',
 		} ).cssValueToBe( `1119px` );
-	} );
-	it( 'assert padding of the verse in the block editor', async () => {
-		await page.waitForSelector( '.wp-block-verse' );
-		await expect( {
-			selector: '.wp-block-verse',
-			property: 'padding-left',
-		} ).cssValueToBe( `20px` );
-		await page.waitForSelector( '.wp-block-verse' );
-		await expect( {
-			selector: '.wp-block-verse',
-			property: 'padding-right',
-		} ).cssValueToBe( `5px` );
-		await page.waitForSelector( '.wp-block-verse' );
-		await expect( {
-			selector: '.wp-block-verse',
-			property: 'padding-top',
-		} ).cssValueToBe( `5px` );
-		await page.waitForSelector( '.wp-block-verse' );
-		await expect( {
-			selector: '.wp-block-verse',
-			property: 'padding-bottom',
-		} ).cssValueToBe( `5px` );
-	} );
-
-	it( 'assert margin of the verse in the block editor', async () => {
-		await page.waitForSelector( '.wp-block-verse' );
-		await expect( {
-			selector: '.wp-block-verse',
-			property: 'margin-top',
-		} ).cssValueToBe( `28px` );
-		await page.waitForSelector( '.wp-block-verse' );
-		await expect( {
-			selector: '.wp-block-verse',
-			property: 'margin-right',
-		} ).cssValueToBe( `0px` );
-		await page.waitForSelector( '.wp-block-verse' );
-		await expect( {
-			selector: '.wp-block-verse',
-			property: 'margin-bottom',
-		} ).cssValueToBe( `28px` );
-		await page.waitForSelector( '.wp-block-verse' );
-		await expect( {
-			selector: '.wp-block-verse',
-			property: 'margin-left',
-		} ).cssValueToBe( `0px` );
 	} );
 } );

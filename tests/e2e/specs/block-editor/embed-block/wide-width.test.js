d5f6ab5cbc5957b7d6e4b56564f84c774d7a8eed
Simran <simranc@bsf.io>
2022-01-12 14:59:09 +0530

    modified code

--- tests/e2e/specs/block-editor/embed-block/wide-width.test.js
+++ tests/e2e/specs/block-editor/embed-block/wide-width.test.js
@@ -1,69 +1,28 @@
 import { insertBlock, createNewPost, clickBlockToolbarButton } from '@wordpress/e2e-test-utils';
 describe( 'Embed in gutenberg editor', () => {
-	it( 'assert wide width of the Embed in the block editor', async () => {
-		await createNewPost( {
-			postType: 'post',
-			title: 'test Embed',
-		} );
+	it( 'assert wide and fulll width of the Embed in the block editor', async () => {
+		await createNewPost( { postType: 'post', title: 'test Embed' } );
 		await insertBlock( 'Embed' );
+		//wide width for Embeds block
 		await clickBlockToolbarButton( 'Align' );
 		await page.waitForFunction( () =>
-			// eslint-disable-next-line @wordpress/no-global-active-element
-			document.activeElement.classList.contains(
-				'components-dropdown-menu__menu-item',
-			),
-		);
-		await page.click(
-			'#editor > div.popover-slot > div > div > div > div > button:nth-child(4)',
-		);
+			document.activeElement.classList.contains( 'components-dropdown-menu__menu-item' ) );
+		await page.click( '[aria-label="Align"] button:nth-child(4)' );
 		await page.waitForSelector( '.wp-block-embed' );
 		await expect( {
 			selector: '.wp-block-embed',
 			property: 'width',
 		} ).cssValueToBe( `1119px` );
-	} );
-	it( 'assert padding of the Embed in the block editor', async () => {
+
+		//full width for Embeds block
+		await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			document.activeElement.classList.contains( 'components-dropdown-menu__menu-item' ) );
+		await page.click( '[aria-label="Align"] button:nth-child(5)' );
 		await page.waitForSelector( '.wp-block-embed' );
 		await expect( {
 			selector: '.wp-block-embed',
-			property: 'padding-left',
-		} ).cssValueToBe( `0px` );
-		await page.waitForSelector( '.wp-block-embed' );
-		await expect( {
-			selector: '.wp-block-embed',
-			property: 'padding-right',
-		} ).cssValueToBe( `0px` );
-		await page.waitForSelector( '.wp-block-embed' );
-		await expect( {
-			selector: '.wp-block-embed',
-			property: 'padding-top',
-		} ).cssValueToBe( `0px` );
-		await page.waitForSelector( '.wp-block-embed' );
-		await expect( {
-			selector: '.wp-block-embed',
-			property: 'padding-bottom',
-		} ).cssValueToBe( `0px` );
-	} );
-	it( 'assert margin of the embed in the block editor', async () => {
-		await page.waitForSelector( '.wp-block-embed' );
-		await expect( {
-			selector: '.wp-block-embed',
-			property: 'margin-top',
-		} ).cssValueToBe( `0px` );
-		await page.waitForSelector( '.wp-block-embed' );
-		await expect( {
-			selector: '.wp-block-embed',
-			property: 'margin-right',
-		} ).cssValueToBe( `0px` );
-		await page.waitForSelector( '.wp-block-embed' );
-		await expect( {
-			selector: '.wp-block-embed',
-			property: 'margin-bottom',
-		} ).cssValueToBe( `15px` );
-		await page.waitForSelector( '.wp-block-embed' );
-		await expect( {
-			selector: '.wp-block-embed',
-			property: 'margin-left',
-		} ).cssValueToBe( `0px` );
+			property: 'width',
+		} ).cssValueToBe( `1119px` );
 	} );
 } );

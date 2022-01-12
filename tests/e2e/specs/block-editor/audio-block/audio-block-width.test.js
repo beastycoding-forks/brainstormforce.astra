ecf31f45a42abea3a3de7c1a36285887bb22b063
Simran <simranc@bsf.io>
2022-01-12 16:32:59 +0530

    added test case for default width

--- tests/e2e/specs/block-editor/audio-block/audio-block-width.test.js
+++ tests/e2e/specs/block-editor/audio-block/audio-block-width.test.js
@@ -1,12 +1,41 @@
-import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
+import { insertBlock, createNewPost, clickBlockToolbarButton } from '@wordpress/e2e-test-utils';
 describe( 'Audio block in gutenberg editor', () => {
-	it( 'assert width of the audio in the block editor', async () => {
+	it( 'assert full and width of the audio in the block editor', async () => {
 		await createNewPost( { postType: 'post', title: 'test audio block' } );
+		await page.click( '[aria-label="Settings"]' );
 		await insertBlock( 'Audio' );
-		await page.waitForSelector( '.components-placeholder.components-placeholder' );
+		//wide width for pullquotaudio block
+		await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			document.activeElement.classList.contains( 'components-dropdown-menu__menu-item' ) );
+		await page.click( '[aria-label="Align"] button:nth-child(4)' );
+		await page.waitForSelector( '.wp-block-audio' );
 		await expect( {
-			selector: '.components-placeholder.components-placeholder',
+			selector: '.wp-block-audio',
 			property: 'width',
-		} ).cssValueToBe( `1119px` );
+		} ).cssValueToBe( `1200px` );
+
+		//full width for audio block
+		await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			document.activeElement.classList.contains( 'components-dropdown-menu__menu-item' ) );
+		await page.click( '[aria-label="Align"] button:nth-child(5)' );
+		await page.waitForSelector( '.wp-block-audio' );
+		await expect( {
+			selector: '.wp-block-audio',
+			property: 'width',
+		} ).cssValueToBe( `1399px` );
+
+		//default width for audio block
+		await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			document.activeElement.classList.contains( 'components-dropdown-menu__menu-item' ) );
+		await page.click( '[aria-label="Align"] button:nth-child(5)' );
+		await page.waitForSelector( '.wp-block-audio' );
+		await page.waitForSelector( '.wp-block-audio' );
+		await expect( {
+			selector: '.wp-block-audio',
+			property: 'width',
+		} ).cssValueToBe( `1256px` );
 	} );
 } );

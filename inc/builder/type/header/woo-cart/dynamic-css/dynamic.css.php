ac273c9154efb59e06d02c410613ea571b92d937
GauravKhupse <gauravk@bsf.io>
2022-01-13 16:58:04 +0530

    Fixed fronted selector changes

--- inc/builder/type/header/woo-cart/dynamic-css/dynamic.css.php
+++ inc/builder/type/header/woo-cart/dynamic-css/dynamic.css.php
@@ -166,7 +166,7 @@ function astra_hb_woo_cart_dynamic_css( $dynamic_css, $dynamic_css_filtered = ''
 			'height' => astra_get_css_value( $cart_icon_size_desktop, 'px' ),
 			'width'  => astra_get_css_value( $cart_icon_size_desktop, 'px' ),
 		),
-		$selector . '.ast-cart-menu-wrap'             => array(
+		'.ast-cart-menu-wrap'             => array(
 			'font-size' => astra_get_css_value( $cart_icon_size_desktop, 'px' ),
 		),
 		$selector . ' a.cart-container *'             => array(
@@ -349,7 +349,7 @@ function astra_hb_woo_cart_dynamic_css( $dynamic_css, $dynamic_css_filtered = ''
 			'height' => astra_get_css_value( $cart_icon_size_mobile, 'px' ),
 			'width'  => astra_get_css_value( $cart_icon_size_mobile, 'px' ),
 		),
-		$responsive_selector . '.ast-cart-menu-wrap'       => array(
+		'.ast-header-break-point.ast-hfb-header .ast-cart-menu-wrap'       => array(
 			'font-size' => astra_get_css_value( $cart_icon_size_mobile, 'px' ),
 		),
 
@@ -419,7 +419,7 @@ function astra_hb_woo_cart_dynamic_css( $dynamic_css, $dynamic_css_filtered = ''
 			'height' => astra_get_css_value( $cart_icon_size_tablet, 'px' ),
 			'width'  => astra_get_css_value( $cart_icon_size_tablet, 'px' ),
 		),
-		$responsive_selector . '.ast-cart-menu-wrap'       => array(
+		'.ast-header-break-point.ast-hfb-header .ast-cart-menu-wrap'       => array(
 			'font-size' => astra_get_css_value( $cart_icon_size_tablet, 'px' ),
 		),
 		/**

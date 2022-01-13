55eb1aedff1dde7795c9716d28a8828ac13169d2
Simran <simranc@bsf.io>
2022-01-13 09:33:58 +0530

    modified code

--- tests/e2e/specs/block-editor/list-block/list-block-width.test.js
+++ tests/e2e/specs/block-editor/list-block/list-block-width.test.js
@@ -1,18 +1,16 @@
 import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
 describe( 'List in gutenberg editor', () => {
-	it( 'assert wide width of the list in the block editor', async () => {
-		await createNewPost( {
-			postType: 'post',
-			title: 'test List',
-		} );
+	it( 'assert default width of the list in the block editor', async () => {
+		await createNewPost( { postType: 'post', title: 'test List' } );
+		await page.click( '[aria-label="Settings"]' );
 		await insertBlock( 'List' );
 		await page.keyboard.type( 'one' );
 		await page.keyboard.press( 'Enter' );
 		await page.keyboard.type( 'two' );
-		await page.waitForSelector( '.editor-styles-wrapper > .block-editor-block-list__layout' );
+		await page.waitForSelector( '.wp-block-list' );
 		await expect( {
-			selector: '.editor-styles-wrapper > .block-editor-block-list__layout',
+			selector: '.wp-block-list',
 			property: 'width',
-		} ).cssValueToBe( `1119px` );
+		} ).cssValueToBe( `1256px` );
 	} );
 } );

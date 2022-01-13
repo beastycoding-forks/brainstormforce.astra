53df5403a198a0f756c9031825f0aa35b49200cd
Simran <simranc@bsf.io>
2022-01-13 12:25:09 +0530

    modified code

--- tests/e2e/specs/block-editor/columns-block/add-another-block-in-column-block.test.js
+++ tests/e2e/specs/block-editor/columns-block/add-another-block-in-column-block.test.js
@@ -1,16 +1,46 @@
-import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
+import { createNewPost, insertBlock, clickBlockToolbarButton } from '@wordpress/e2e-test-utils';
 describe( 'columns in gutenberg editor', () => {
 	it( 'add other blocks in column block and assert width', async () => {
 		await createNewPost( { postType: 'post', title: 'test columns' } );
+		await page.click( '[aria-label="Settings"]' );
 		await insertBlock( 'Columns' );
 		await page.click( '[aria-label="One column"]' );
 		await page.click( '.block-editor-button-block-appender' );
 		await page.click( '.editor-block-list-item-paragraph' );
 		await page.keyboard.type( 'Columns Block with a Paragraph' );
-		await page.waitForSelector( '.editor-styles-wrapper > .block-editor-block-list__layout' );
+		await clickBlockToolbarButton( 'Select Column' );
+		await clickBlockToolbarButton( 'Select Columns' );
+		//Set wide width for the column block with paragraph.
+		await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			document.activeElement.classList.contains( 'components-dropdown-menu__menu-item' ) );
+		await page.click( '[aria-label="Align"] button:nth-child(1)' );
+		await page.waitForSelector( '.wp-block-columns' );
 		await expect( {
-			selector: '.editor-styles-wrapper > .block-editor-block-list__layout',
+			selector: '.wp-block-columns',
 			property: 'width',
-		} ).cssValueToBe( `1119px` );
+		} ).cssValueToBe( `1200px` );
+
+		//Set full width for the column block with paragraph.
+		await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			document.activeElement.classList.contains( 'components-dropdown-menu__menu-item' ) );
+		await page.click( '[aria-label="Align"] button:nth-child(2)' );
+		await page.waitForSelector( '.wp-block-columns' );
+		await expect( {
+			selector: '.wp-block-columns',
+			property: 'width',
+		} ).cssValueToBe( `1399px` );
+
+		//Set full default for the column block with paragraph.
+		await clickBlockToolbarButton( 'Align' );
+		await page.waitForFunction( () =>
+			document.activeElement.classList.contains( 'components-dropdown-menu__menu-item' ) );
+		await page.click( '[aria-label="Align"] button:nth-child(2)' );
+		await page.waitForSelector( '.wp-block-columns' );
+		await expect( {
+			selector: '.wp-block-columns',
+			property: 'width',
+		} ).cssValueToBe( `910px` );
 	} );
 } );

cd8df3d76545b3b71cefbf1673ed366db5ec148b
Navanath Bhosale <navanathb@bsf.io>
2022-01-14 15:28:32 +0530

    chore: resolved all PR comments & adjusted code in suggessions

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '04695bef3b2bb5f91e94a69fc93fb5d9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'a79d8f835e266d2fa17bcac811251ae4');

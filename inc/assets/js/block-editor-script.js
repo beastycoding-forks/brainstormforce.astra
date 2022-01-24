e9db8190f77ebc12344f6de4630079ffbf473132
Navanath Bhosale <navanathb@bsf.io>
2022-01-24 12:13:55 +0530

    fix: var() colors code appearing in block editor theme palette picker.

--- inc/assets/js/block-editor-script.js
+++ inc/assets/js/block-editor-script.js
@@ -53,6 +53,18 @@ function astra_onload_function() {
 
 	wp.data.subscribe(function () {
 		setTimeout( function () {
+			/**
+			 * In WP-5.9 block editor comes up with color palette showing color-code canvas, but with theme var() CSS its appearing directly as it is. So updated them on wp.data event.
+			 */
+			var customColorPickerButtons = document.querySelectorAll( '.components-color-palette__custom-color' );
+
+			for ( var item = 0;  item < customColorPickerButtons.length; item++ ) {
+				var colorCode = customColorPickerButtons[item].innerText;
+				if ( colorCode.indexOf( 'var(--ast-global-color' ) > -1 ) {
+					customColorPickerButtons[item].innerHTML = astraColors[ colorCode ];
+				}
+			}
+
 			var spacerBlocks = document.querySelectorAll( '.wp-block.wp-block-spacer' );
 
 			for ( var item = 0;  item < spacerBlocks.length; item++ ) {

f7b5386c6bc5176b67c0a93666473a7acd07f5d3
Nikhil <email@nikhilchavan.com>
2022-02-21 12:00:15 +0530

    set color profile to srgb

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,6 +3,6 @@ const config = require( '@wordpress/scripts/config/puppeteer.config' );
 module.exports = {
 	launch: {
 		...config,
-		args: [ `${ config.launch.args[ 0 ] } --enable-font-antialiasing=false --disable-font-subpixel-positioning --force-color-profile=generic-rgb --font-render-hinting=none` ],
+		args: [ `${ config.launch.args[ 0 ] } --enable-font-antialiasing=false --disable-font-subpixel-positioning --force-color-profile=srgb --font-render-hinting=none` ],
 	},
 };

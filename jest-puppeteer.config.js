b05ad28e640a17d9d15d27c298534ee814a27016
Nikhil <email@nikhilchavan.com>
2022-02-22 16:52:57 +0530

    Force firefox for tests in puppeteer

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,7 @@
 const config = require( '@wordpress/scripts/config/puppeteer.config' );
 
 module.exports = {
+	browser: 'firefox',
 	launch: {
 		...config,
 		args: [ `${ config.launch.args[ 0 ] } --enable-font-antialiasing=false --disable-font-subpixel-positioning --force-color-profile=srgb --font-render-hinting=none` ],

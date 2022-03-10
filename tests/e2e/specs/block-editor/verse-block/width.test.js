bc5290db4e760e9d5334cb1333a4ae640a009598
Simran <simranc@bsf.io>
2022-03-10 14:48:24 +0530

    cahnged width

--- tests/e2e/specs/block-editor/verse-block/width.test.js
+++ tests/e2e/specs/block-editor/verse-block/width.test.js
@@ -2,12 +2,13 @@ import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
 describe( 'verse in gutenberg editor', () => {
 	it( 'assert default width of the verse in the block editor', async () => {
 		await createNewPost( { postType: 'post', title: 'test verse' } );
+		await page.click( '[aria-label="Settings"]' );
 		await insertBlock( 'Verse' );
 		//default width for verse block
 		await page.waitForSelector( '.wp-block-verse' );
 		await expect( {
 			selector: '.wp-block-verse',
 			property: 'width',
-		} ).cssValueToBe( `1119px` );
+		} ).cssValueToBe( `910px` );
 	} );
 } );

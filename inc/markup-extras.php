7bc044eecfc025b0ee83096801b46cee9d0cced9
GauravKhupse <gauravk@bsf.io>
2022-03-11 09:38:20 +0530

    Optimise variable changes

--- inc/markup-extras.php
+++ inc/markup-extras.php
@@ -252,11 +252,10 @@ function astra_get_site_title_tagline( $display_site_title, $display_site_taglin
 		 * @param string the HTML output for Site Title.
 		 */
 		// Site Title.
-		$site_title_markup     = '';
-		$header_builder_active = Astra_Builder_Helper::$is_header_footer_builder_active;
-		$site_title_setting    = astra_get_option( 'display-site-title-responsive' );
+		$site_title_markup  = '';
+		$site_title_setting = astra_get_option( 'display-site-title-responsive' );
 		foreach ( $site_title_setting  as $specific_device => $val ) {
-			if ( false === $header_builder_active || is_customize_preview() || ( 1 === $val || true === $val ) && ( $device === $specific_device ) ) {
+			if ( false === Astra_Builder_Helper::$is_header_footer_builder_active || is_customize_preview() || ( 1 === $val || true === $val ) && ( $device === $specific_device ) ) {
 				$site_title_markup = apply_filters(
 					'astra_site_title_output',
 					sprintf(

2218427398555b7481d67be2f74784a791a936c7
Simran <simranc@bsf.io>
2022-03-15 14:28:02 +0530

    modified code

--- tests/e2e/specs/block-editor/pullquote-block/width.test.js
+++ tests/e2e/specs/block-editor/pullquote-block/width.test.js
@@ -1,10 +1,11 @@
 import { insertBlock, createNewPost, clickBlockToolbarButton } from '@wordpress/e2e-test-utils';
-describe( 'Pullquote in gutenberg editor', () => {
+describe( 'pullquote in gutenberg editor', () => {
 	it( 'assert wide, full and default width of the pullquote in the block editor', async () => {
 		await createNewPost( { postType: 'post', title: 'test Pullquote' } );
 		await page.click( '[aria-label="Settings"]' );
 		await insertBlock( 'Pullquote' );
 		await page.keyboard.type( 'Testing full, wide and default width' );
+
 		//wide width for pullquote block
 		await clickBlockToolbarButton( 'Align' );
 		await page.waitForFunction( () =>
@@ -14,24 +15,24 @@ describe( 'Pullquote in gutenberg editor', () => {
 		await expect( {
 			selector: '.wp-block-pullquote',
 			property: 'width',
-		} ).cssValueToBe( `1200px` );
+		} ).cssValueToBe( `1399px` );
 
 		//full width for pullquote block
 		await clickBlockToolbarButton( 'Align' );
 		await page.waitForFunction( () =>
 			document.activeElement.classList.contains( 'components-dropdown-menu__menu-item' ) );
-		await page.click( '[aria-label="Align"] button:nth-child(4)' );
+		await page.click( '[aria-label="Align"] button:nth-child(2)' );
 		await page.waitForSelector( '.wp-block-pullquote' );
 		await expect( {
 			selector: '.wp-block-pullquote',
 			property: 'width',
-		} ).cssValueToBe( `1399px` );
+		} ).cssValueToBe( `1200px` );
 
 		//default width for pullquote block
 		await clickBlockToolbarButton( 'Align' );
 		await page.waitForFunction( () =>
 			document.activeElement.classList.contains( 'components-dropdown-menu__menu-item' ) );
-		await page.click( '[aria-label="Align"] button:nth-child(4)' );
+		await page.click( '[aria-label="Align"] button:nth-child(1)' );
 		await page.waitForSelector( '.wp-block-pullquote' );
 		await expect( {
 			selector: '.wp-block-pullquote',

13e38446740ee09a3e9ce605471abc801b91f59d
Simran <simranc@bsf.io>
2022-03-16 09:54:50 +0530

    test case for lifter lms layout option in global container customizer section

---
+++ tests/e2e/specs/customizer/lifter-lms/layout.test.js
@@ -0,0 +1,24 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/customize';
+describe( 'lifter lms layout setting from customizer', () => {
+	it( 'layout should apply', async () => {
+		await page.goto( createURL( '/wp-admin' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.click( '#toplevel_page_lifterlms > a > div.wp-menu-name' );
+		await page.click( '#toplevel_page_lifterlms > ul > li:nth-child(5) > a' );
+		await page.click( '#wpbody-content > div.wrap.lifterlms.llms-import-export > form > ul > li:nth-child(2) > button' );
+		const lifterLMSLayout = {
+			'lifterlms-content-layout': 'boxed-container',
+		};
+		await setCustomize( lifterLMSLayout );
+		await page.goto( createURL( '/course/free-course-lead-magnet-template-2' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-container' );
+		await expect( {
+			selector: '.ast-container',
+			property: 'max-width',
+		} ).cssValueToBe( `${ lifterLMSLayout[ 'shop-archive-max-width' ] }` );
+	} );
+} ); 

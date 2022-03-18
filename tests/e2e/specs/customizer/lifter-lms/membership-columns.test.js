e2f6e7bcf4f7b2dbfa61eb76abd0171a5467383d
Simran <simranc@bsf.io>
2022-03-18 17:13:36 +0530

    test case for lifter lms membership columns

---
+++ tests/e2e/specs/customizer/lifter-lms/membership-columns.test.js
@@ -0,0 +1,28 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/customize';
+describe( 'lifter lms course columns setting from customizer', () => {
+	it( 'membership columns should apply', async () => {
+		await page.goto( createURL( '/wp-admin' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.hover( '#toplevel_page_lifterlms > a > div.wp-menu-name' );
+		await page.click( '.toplevel_page_lifterlms ul li:nth-child(5)' );
+		await page.click( '#wpbody-content > div.wrap.lifterlms.llms-import-export > form > ul > li:nth-child(2) > button' );
+		const lifterLmsColumns = {
+			'llms-membership-grid': {
+				desktop: 4,
+				tablet: 3,
+				mobile: 2,
+			},
+		};
+		await setCustomize( lifterLmsColumns );
+		await page.goto( createURL( '/memberships' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.llms-loop-list.cols-3 .llms-loop-item' );
+		await expect( {
+			selector: '.llms-loop-list.cols-3 .llms-loop-item',
+			property: 'width',
+		} ).cssValueToBe( `${ lifterLmsColumns[ 'llms-membership-grid' ].desktop + '%' }` );
+	} );
+} );

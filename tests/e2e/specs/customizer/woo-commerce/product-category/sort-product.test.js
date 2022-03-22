09aad459f8353454b9f3f62b4ef287300497c6df
Snehal Kolhe <snehalk@bsf.io>
2022-03-22 12:59:30 +0530

    modified test

--- tests/e2e/specs/customizer/woo-commerce/product-category/sort-product.test.js
+++ tests/e2e/specs/customizer/woo-commerce/product-category/sort-product.test.js
@@ -1,11 +1,11 @@
 import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Sort the products by multiple options from customizer', () => {
-	it( 'should sort the products', async () => {
-		const categoryDisplay = {
-			'_customize-input-woocommerce_default_catalog_orderby': 'Show products',
+	it( 'should sort the product by default', async () => {
+		const sortProduct = {
+			'_customize-input-woocommerce_default_catalog_orderby': 'Default sorting (custom ordering + name)',
 		};
-		await setCustomize( categoryDisplay );
+		await setCustomize( sortProduct );
 		await page.goto( createURL( 'Shop' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -15,11 +15,11 @@ describe( 'Sort the products by multiple options from customizer', () => {
 			property: 'display',
 		} ).cssValueToBe( `inline-block` );
 	} );
-	it( 'should set the category display as Show subcategories', async () => {
-		const categoryDisplay = {
-			'_customize-input-woocommerce_default_catalog_orderby': 'Show subcategories',
+	it( 'should sort the product by popularity', async () => {
+		const sortProduct = {
+			'_customize-input-woocommerce_default_catalog_orderby': 'Popularity (sales)',
 		};
-		await setCustomize( categoryDisplay );
+		await setCustomize( sortProduct );
 		await page.goto( createURL( 'Shop' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -29,11 +29,53 @@ describe( 'Sort the products by multiple options from customizer', () => {
 			property: 'display',
 		} ).cssValueToBe( `inline-block` );
 	} );
-	it( 'should set the category display as Show subcategories & products', async () => {
-		const categoryDisplay = {
-			'_customize-input-woocommerce_default_catalog_orderby': 'Show subcategories & products',
+	it( 'should sort the product by average rating', async () => {
+		const sortProduct = {
+			'_customize-input-woocommerce_default_catalog_orderby': 'Average rating',
 		};
-		await setCustomize( categoryDisplay );
+		await setCustomize( sortProduct );
+		await page.goto( createURL( 'Shop' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.woocommerce ul.products' );
+		await expect( {
+			selector: '.woocommerce ul.products',
+			property: 'list-style',
+		} ).cssValueToBe( `inline-block` );
+	} );
+	it( 'should sort the product by most recent', async () => {
+		const sortProduct = {
+			'_customize-input-woocommerce_default_catalog_orderby': 'Sort by most recent',
+		};
+		await setCustomize( sortProduct );
+		await page.goto( createURL( 'Shop' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.woocommerce ul.products' );
+		await expect( {
+			selector: '.woocommerce ul.products',
+			property: 'list-style',
+		} ).cssValueToBe( `inline-block` );
+	} );
+	it( 'should sort the product by ascending order', async () => {
+		const sortProduct = {
+			'_customize-input-woocommerce_default_catalog_orderby': 'Sort by price (asc)',
+		};
+		await setCustomize( sortProduct );
+		await page.goto( createURL( 'Shop' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.woocommerce ul.products' );
+		await expect( {
+			selector: '.woocommerce ul.products',
+			property: 'list-style',
+		} ).cssValueToBe( `inline-block` );
+	} );
+	it( 'should sort the product by descending order', async () => {
+		const sortProduct = {
+			'_customize-input-woocommerce_default_catalog_orderby': 'Sort by price (desc)',
+		};
+		await setCustomize( sortProduct );
 		await page.goto( createURL( 'Shop' ), {
 			waitUntil: 'networkidle0',
 		} );

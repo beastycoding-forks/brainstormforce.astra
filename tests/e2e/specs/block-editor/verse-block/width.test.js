3a07d28d8cf668b797679fe280cf3350459b9248
Snehal Kolhe <snehalk@bsf.io>
2022-03-24 12:35:24 +0530

    modified description

--- tests/e2e/specs/block-editor/verse-block/width.test.js
+++ tests/e2e/specs/block-editor/verse-block/width.test.js
@@ -1,7 +1,7 @@
 import { insertBlock, createNewPost } from '@wordpress/e2e-test-utils';
-describe( 'verse in gutenberg editor', () => {
-	it( 'assert default width of the verse in the block editor', async () => {
-		await createNewPost( { postType: 'post', title: 'test verse' } );
+describe( 'Verse in gutenberg editor', () => {
+	it( 'assert default width of the verse block in the block editor', async () => {
+		await createNewPost( { postType: 'post', title: 'test-sverse' } );
 		await page.click( '[aria-label="Settings"]' );
 		await insertBlock( 'Verse' );
 		//default width for verse block

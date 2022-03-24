c80b685197ba412d0e4ffbacfa39b6539c521bfe
nileshc-bsf <nileshc@bsf.io>
2022-03-24 19:41:22 +0530

    Fix: Offcanvas Shopping cart closed automatically on mobile when scrolled.

--- assets/js/unminified/mobile-cart.js
+++ assets/js/unminified/mobile-cart.js
@@ -121,14 +121,6 @@
 
 	}
 
-	window.addEventListener('resize', function () {
-		// Close Cart
-		var cart_close = document.querySelector('.astra-cart-drawer-close');
-		if ( undefined !== cart_close && '' !== cart_close && null !== cart_close && 'INPUT' !== document.activeElement.tagName && cart_flyout.classList.contains( 'active' ) ) {
-			cart_close.click();
-		}
-	});
-
 	window.addEventListener('load', function () {
 		cartInit();
 	});

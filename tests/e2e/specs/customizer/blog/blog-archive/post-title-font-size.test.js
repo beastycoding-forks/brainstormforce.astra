f6c9054307d318b0f920e9a0270ad7c873256713
Simran <simranc@bsf.io>
2022-03-25 17:10:19 +0530

    modified publishPost(), modified codE

--- tests/e2e/specs/customizer/blog/blog-archive/post-title-font-size.test.js
+++ tests/e2e/specs/customizer/blog/blog-archive/post-title-font-size.test.js
@@ -1,55 +1,53 @@
-import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
+import { publishPost } from '../../../../utils/publish-post';
 import { setCustomize } from '../../../../utils/customize';
 import { responsiveFontSize } from '../../../../utils/responsive-utils';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 describe( 'Blog Archive option under the customizer', () => {
 	it( 'blog Archive post font size options should apply correctly', async () => {
-		const bposttitlefontsize = {
+		const blogPostTitleFontSize = {
 			'font-size-page-title': {
-				desktop: '50',
-				tablet: '50',
-				mobile: '50',
+				desktop: '60',
+				tablet: '40',
+				mobile: '20',
 				'desktop-unit': 'px',
 				'tablet-unit': 'px',
 				'mobile-unit': 'px',
 			},
 		};
-		await setCustomize( bposttitlefontsize );
-		await createNewPost( {
-			postType: 'post',
-			title: 'sample-page',
-
-		} );
-		await publishPost();
-
+		await setCustomize( blogPostTitleFontSize );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'post', title: 'Blog-post' } );
+			ppStatus = await publishPost();
+		}
 		await page.goto( createURL( '/author/admin' ), {
 			waitUntil: 'networkidle0',
 		} );
-
-		await page.waitForSelector( '.archive .entry-title' );
+		await page.waitForSelector( '.entry-title' );
 		await expect( {
 			selector: '.entry-title',
 			property: 'font-size',
-		} ).cssValueToBe( `${ bposttitlefontsize[ 'font-size-page-title' ].desktop }${ bposttitlefontsize[ 'font-size-page-title' ][ 'desktop-unit' ] }` );
+		} ).cssValueToBe( `${ blogPostTitleFontSize[ 'font-size-page-title' ].desktop }${ blogPostTitleFontSize[ 'font-size-page-title' ][ 'desktop-unit' ] }` );
 
 		await setBrowserViewport( 'medium' );
 		await expect( {
-			selector: '.archive .entry-title',
+			selector: '.entry-title',
 			property: 'font-size',
 		} ).cssValueToBe(
 			`${ await responsiveFontSize(
-				bposttitlefontsize[ 'font-size-page-title' ].tablet,
-			) }${ bposttitlefontsize[ 'font-size-page-title' ][ 'tablet-unit' ] }`,
+				blogPostTitleFontSize[ 'font-size-page-title' ].tablet,
+			) }${ blogPostTitleFontSize[ 'font-size-page-title' ][ 'tablet-unit' ] }`,
 		);
 
 		await setBrowserViewport( 'small' );
 		await expect( {
-			selector: '.archive .entry-title',
+			selector: '.entry-title',
 			property: 'font-size',
 		} ).cssValueToBe(
 			`${ await responsiveFontSize(
-				bposttitlefontsize[ 'font-size-page-title' ].mobile,
-			) }${ bposttitlefontsize[ 'font-size-page-title' ][ 'mobile-unit' ] }`,
+				blogPostTitleFontSize[ 'font-size-page-title' ].mobile,
+			) }${ blogPostTitleFontSize[ 'font-size-page-title' ][ 'mobile-unit' ] }`,
 		);
 	} );
 } );

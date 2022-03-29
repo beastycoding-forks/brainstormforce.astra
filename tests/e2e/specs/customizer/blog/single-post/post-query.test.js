97889de14d0732caa8072df521b56dcb4cf58f91
Simran <simranc@bsf.io>
2022-03-29 13:59:28 +0530

    resolved PR comments

--- tests/e2e/specs/customizer/blog/single-post/post-query.test.js
+++ tests/e2e/specs/customizer/blog/single-post/post-query.test.js
@@ -1,90 +1,73 @@
-import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
+import { publishPost } from '../../../../utils/publish-post';
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Single post option under the customizer', () => {
-	it( 'post query Related post by by option should apply correctly', async () => {
-		const GridandColumns = {
-			'enable-related-posts': true,
+	it( 'post query based on option should apply correctly', async () => {
+		const gridAndColumns = {
+			'enable-related-posts': 1,
 			'related-posts-based-on': 'categories',
 		};
-		await setCustomize( GridandColumns );
-		await createNewPost( {
-			postType: 'post',
-			title: 'a-post',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'post',
-			title: 'b-post',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'post',
-			title: 'c-post',
-		} );
-		await publishPost();
-		await page.goto( createURL( 'a-post' ), {
+		await setCustomize( gridAndColumns );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'post', title: 'a-post' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'b-post' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'c-post' } );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/a-post' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.evaluate( () => {
 			window.scrollBy( 0, window.innerHeight );
 		} );
 		await page.waitForSelector( '.ast-related-posts-wrapper' );
-		const Relatedpostby = await page.$eval( '.ast-related-posts-wrapper', ( element ) => element.getAttribute( '.ast-single-related-posts-container .ast-related-posts-wrapper' ) );
-		await expect( Relatedpostby ).toBeNull( );
+		const relatedPostBy = await page.$eval( '.ast-related-posts-wrapper', ( element ) => element.getAttribute( '.ast-single-related-posts-container .ast-related-posts-wrapper' ) );
+		await expect( relatedPostBy ).toBeNull( );
 	} );
 	it( 'post query Order by option should apply correctly', async () => {
-		const GridandColumns = {
-			'enable-related-posts': true,
+		const gridAndColumns = {
+			'enable-related-posts': 1,
 			'related-posts-order-by': 'title',
 		};
-		await setCustomize( GridandColumns );
-		await createNewPost( {
-			postType: 'post',
-			title: 'a-post',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'post',
-			title: 'b-post',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'post',
-			title: 'c-post',
-		} );
-		await publishPost();
-		await page.goto( createURL( 'a-post' ), {
+		await setCustomize( gridAndColumns );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'post', title: 'a-post' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'b-post' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'c-post' } );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/a-post' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.evaluate( () => {
 			window.scrollBy( 0, window.innerHeight );
 		} );
 		await page.waitForSelector( '.ast-related-posts-wrapper' );
-		const Orderby = await page.$eval( '.ast-related-posts-wrapper', ( element ) => element.getAttribute( '.ast-single-related-posts-container .ast-related-posts-wrapper' ) );
-		await expect( Orderby ).toBeNull( );
+		const orderBy = await page.$eval( '.ast-related-posts-wrapper', ( element ) => element.getAttribute( '.ast-single-related-posts-container .ast-related-posts-wrapper' ) );
+		await expect( orderBy ).toBeNull( );
 	} );
 	it( 'post query Order option should apply correctly', async () => {
-		const GridandColumns = {
-			'enable-related-posts': true,
+		const gridAndColumns = {
+			'enable-related-posts': 1,
 			'related-posts-order': 'desc',
 		};
-		await setCustomize( GridandColumns );
-		await createNewPost( {
-			postType: 'post',
-			title: 'a-post',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'post',
-			title: 'b-post',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'post',
-			title: 'c-post',
-		} );
-		await publishPost();
-		await page.goto( createURL( 'a-post' ), {
+		await setCustomize( gridAndColumns );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'post', title: 'a-post' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'b-post' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'c-post' } );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/a-post' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.evaluate( () => {

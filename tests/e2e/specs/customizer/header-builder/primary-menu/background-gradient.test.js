33f333be8c4e3a5bfaedc6d287591f74ba981fda
Snehal Kolhe <snehalk@bsf.io>
2022-04-21 13:18:25 +0530

    fixed timeout error

--- tests/e2e/specs/customizer/header-builder/primary-menu/background-gradient.test.js
+++ tests/e2e/specs/customizer/header-builder/primary-menu/background-gradient.test.js
@@ -1,8 +1,10 @@
-import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
-import { publishPost } from '../../../../utils/publish-post';
+import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
+import { createNewMenu } from '../../../../utils/create-menu';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 describe( 'Primary menu background gradient color settings in the customizer', () => {
 	it( 'background gradient color should apply correctly', async () => {
+		await createNewMenu();
 		const primaryMenuGradient = {
 			'header-menu1-bg-obj-responsive': {
 				desktop: {
@@ -13,14 +15,25 @@ describe( 'Primary menu background gradient color settings in the customizer', (
 					'background-attachment': 'fixed',
 					'background-type': 'gradient',
 				},
+				tablet: {
+					'background-color': 'linear-gradient(135deg, rgb(195, 221, 236) 0%, rgb(155, 81, 224) 100%)',
+					'background-repeat': 'no-repeat',
+					'background-position': 'left top',
+					'background-size': 'contain',
+					'background-attachment': 'fixed',
+					'background-type': 'gradient',
+				},
+				mobile: {
+					'background-color': 'linear-gradient(135deg, rgb(162, 242, 168) 0%, rgb(229, 200, 201) 100%)',
+					'background-repeat': 'no-repeat',
+					'background-position': 'left top',
+					'background-size': 'contain',
+					'background-attachment': 'fixed',
+					'background-type': 'gradient',
+				},
 			},
 		};
 		await setCustomize( primaryMenuGradient );
-		let ppStatus = false;
-		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'page', title: 'test-1' } );
-			ppStatus = await publishPost();
-		}
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -29,5 +42,23 @@ describe( 'Primary menu background gradient color settings in the customizer', (
 			selector: '.ast-builder-menu-1 .main-header-menu',
 			property: 'background-image',
 		} ).cssValueToBe( `${ primaryMenuGradient[ 'header-menu1-bg-obj-responsive' ].desktop[ 'background-color' ] }` );
+
+		await setBrowserViewport( 'medium' );
+		await page.click(
+			'.ast-mobile-header-wrap .ast-button-wrap .menu-toggle.main-header-menu-toggle',
+		);
+		await expect( {
+			selector: '.ast-builder-menu-1 .main-header-menu',
+			property: 'background-image',
+		} ).cssValueToBe( `${ primaryMenuGradient[ 'header-menu1-bg-obj-responsive' ].tablet[ 'background-color' ] }` );
+
+		await setBrowserViewport( 'small' );
+		await page.click(
+			'.ast-mobile-header-wrap .ast-button-wrap .menu-toggle.main-header-menu-toggle',
+		);
+		await expect( {
+			selector: '.ast-builder-menu-1 .main-header-menu',
+			property: 'background-image',
+		} ).cssValueToBe( `${ primaryMenuGradient[ 'header-menu1-bg-obj-responsive' ].mobile[ 'background-color' ] }` );
 	} );
 } );

c4d42638a6f9dd4453939fb645b5de2ad0d7d3a1
Simran <simranc@bsf.io>
2022-04-22 17:29:47 +0530

    E2E test case for submenu width option

---
+++ tests/e2e/specs/customizer/header-builder/primary-menu-submenu/width.test.js
@@ -0,0 +1,27 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { createNewMenu } from '../../../../utils/create-menu';
+import { setCustomize } from '../../../../utils/customize';
+describe( 'Primary menu submenu option under the customizer', () => {
+	it( 'width option should apply correctly', async () => {
+		await createNewMenu();
+		const submenuWidth = {
+			'header-menu1-submenu-width': '100',
+			'header-desktop-items': {
+				primary: {
+					primary_right: {
+						0: 'menu-1',
+					},
+				},
+			},
+		};
+		await setCustomize( submenuWidth );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.hover( '.menu-link' );
+		await expect( {
+			selector: '.ast-builder-menu-1 .sub-menu',
+			property: 'width',
+		} ).cssValueToBe( `${ submenuWidth[ 'header-menu1-submenu-width' ] + 'px' }` );
+	} );
+} );

fffc49515709ba04c8b939bf52c2974960a3bbc2
Simran <simranc@bsf.io>
2022-04-26 09:27:58 +0530

    resolved code analysis

--- tests/e2e/utils/wc-sample-data.js
+++ tests/e2e/utils/wc-sample-data.js
@@ -32,7 +32,7 @@ export async function importProducts(
 
 	await page.click( 'button[type="submit"]' );
 	await scrollToElement( '#collapse-button' );
-	await page.click( 'button[type="submit"]' );
+	await page.click( 'button[value="Run the importer"]' );
 	await page.waitFor( 2000 );
 	await page.click( '.woocommerce-progress-form-wrapper .button' );
 }

9c4687f928a65017e0bbc2b6ddd368a8bbfe61cb
Simran <simranc@bsf.io>
2022-04-26 11:09:41 +0530

    added code for line height multiply by font size and responsive font size

--- tests/e2e/specs/customizer/global/typography/preset4.test.js
+++ tests/e2e/specs/customizer/global/typography/preset4.test.js
@@ -3,6 +3,7 @@ import { publishPost } from '../../../../utils/publish-post';
 import { setCustomize } from '../../../../utils/customize';
 import { TPOGRAPHY_TEST_POST_CONTENT } from '../../../../utils/post';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
+import { responsiveFontSize } from '../../../../utils/responsive-utils';
 describe( 'Global typography settings in the customizer', () => {
 	it( 'preset settings should be applied correctly', async () => {
 		const presetFont = {
@@ -17,11 +18,11 @@ describe( 'Global typography settings in the customizer', () => {
 			},
 			'body-font-weight': '400',
 			'body-text-transform': 'uppercase',
-			'body-line-height': '25px',
+			'body-line-height': '2',
 			'headings-font-family': 'Montserrat, sans-serif',
 			'headings-font-weight': '700',
 			'headings-text-transform': 'lowercase',
-			'headings-line-height': '40px',
+			'headings-line-height': '2',
 		};
 
 		await setCustomize( presetFont );
@@ -49,13 +50,25 @@ describe( 'Global typography settings in the customizer', () => {
 		await expect( {
 			selector: 'body',
 			property: 'font-size',
-		} ).cssValueToBe( `${ presetFont[ 'font-size-body' ].tablet }${ presetFont[ 'font-size-body' ][ 'tablet-unit' ] }` );
+		} ).cssValueToBe(
+			`${ await responsiveFontSize(
+				presetFont[ 'font-size-body' ].tablet,
+			) }${
+				presetFont[ 'font-size-body' ][ 'tablet-unit' ]
+			}`,
+		);
 
 		await setBrowserViewport( 'small' );
 		await expect( {
 			selector: 'body',
 			property: 'font-size',
-		} ).cssValueToBe( `${ presetFont[ 'font-size-body' ].mobile }${ presetFont[ 'font-size-body' ][ 'mobile-unit' ] }` );
+		} ).cssValueToBe(
+			`${ await responsiveFontSize(
+				presetFont[ 'font-size-body' ].mobile,
+			) }${
+				presetFont[ 'font-size-body' ][ 'mobile-unit' ]
+			}`,
+		);
 
 		await expect( {
 			selector: 'body',
@@ -70,7 +83,7 @@ describe( 'Global typography settings in the customizer', () => {
 		await expect( {
 			selector: 'body',
 			property: 'line-height',
-		} ).cssValueToBe( `${ presetFont[ 'body-line-height' ] }` );
+		} ).cssValueToBe( `${ presetFont[ 'body-line-height' ] * presetFont[ 'font-size-body' ].desktop }` + 'px' );
 
 		await expect( {
 			selector: '.entry-content h1',
@@ -90,6 +103,6 @@ describe( 'Global typography settings in the customizer', () => {
 		await expect( {
 			selector: '.entry-content h1',
 			property: 'line-height',
-		} ).cssValueToBe( `${ presetFont[ 'headings-line-height' ] }` );
+		} ).cssValueToBe( `${ presetFont[ 'headings-line-height' ] * presetFont[ 'font-size-body' ].desktop }` + 'px' );
 	} );
 } );

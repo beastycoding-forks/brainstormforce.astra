7b7ce75184358432594cfbd962f124d0fca3e3e2
Simran <simranc@bsf.io>
2022-04-27 13:53:05 +0530

    resolved accessibility error

--- tests/e2e/specs/customizer/footer-builder/html-2/html2-link-color.test.js
+++ tests/e2e/specs/customizer/footer-builder/html-2/html2-link-color.test.js
@@ -7,18 +7,9 @@ describe( 'Footer html2 block settings in the customizer', () => {
 		const html2LinkColor = {
 			'footer-html-2': '<a href="https://wpastra.com/">HTML2 link color</a>',
 			'footer-html-2link-color': {
-				desktop: 'rgb(16, 190, 68)',
-				tablet: 'rgb(193, 106, 16)',
-				mobile: 'rgb(174, 12, 171)',
+				desktop: 'rgb(130, 67, 8)',
 			},
 			'footer-desktop-items': {
-				primary: {
-					primary_2: {
-						0: 'html-2',
-					},
-				},
-			},
-			'footer-mobile-items': {
 				primary: {
 					primary_2: {
 						0: 'html-2',
@@ -37,14 +28,53 @@ describe( 'Footer html2 block settings in the customizer', () => {
 			selector: '.footer-widget-area[data-section="section-fb-html-2"] a',
 			property: 'color',
 		} ).cssValueToBe( `${ html2LinkColor[ 'footer-html-2link-color' ].desktop }` );
+	} );
 
+	it( 'footer html2 link color for tablet should apply correctly', async () => {
+		const html2LinkColor = {
+			'footer-html-2': '<a href="https://wpastra.com/">HTML2 link color</a>',
+			'footer-html-2link-color': {
+				tablet: 'rgb(171, 7, 78)',
+			},
+			'footer-desktop-items': {
+				primary: {
+					primary_2: {
+						0: 'html-2',
+					},
+				},
+			},
+		};
+		await setCustomize( html2LinkColor );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.footer-widget-area[data-section="section-fb-html-2"] a' );
 		await expect( {
 			selector: '.footer-widget-area[data-section="section-fb-html-2"] a',
 			property: 'color',
 		} ).cssValueToBe( `${ html2LinkColor[ 'footer-html-2link-color' ].tablet }` );
+	} );
 
+	it( 'footer html2 link color for mobile should apply correctly', async () => {
+		const html2LinkColor = {
+			'footer-html-2': '<a href="https://wpastra.com/">HTML2 link color</a>',
+			'footer-html-2link-color': {
+				mobile: 'rgb(3, 124, 72)',
+			},
+			'footer-desktop-items': {
+				primary: {
+					primary_2: {
+						0: 'html-2',
+					},
+				},
+			},
+		};
+		await setCustomize( html2LinkColor );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
 		await expect( {

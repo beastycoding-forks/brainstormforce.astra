44302f5d5e863a7af53c0b6a5ae28a4c094047c9
Simran <simranc@bsf.io>
2022-04-28 17:21:48 +0530

    added test case primary menu background image for tablet and mobile

--- tests/e2e/specs/customizer/header-builder/primary-menu/background-image.test.js
+++ tests/e2e/specs/customizer/header-builder/primary-menu/background-image.test.js
@@ -1,8 +1,10 @@
 import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { createNewMenu } from '../../../../utils/create-menu';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 describe( 'Primary menu settings in the customizer', () => {
-	it( 'background image should apply corectly', async () => {
+	it( 'background image should apply correctly', async () => {
+		await createNewMenu();
 		const primaryMenuImage = {
 			'header-menu1-bg-obj-responsive': {
 				desktop: {
@@ -14,20 +16,27 @@ describe( 'Primary menu settings in the customizer', () => {
 					'background-type': 'image',
 				},
 				tablet: {
-					'background-image': '',
-					'background-repeat': '',
-					'background-position': '',
-					'background-size': '',
-					'background-attachment': '',
-					'background-type': '',
+					'background-image': 'https://pd.w.org/2022/03/359622bb65ece8859.46427938-300x200.jpeg',
+					'background-repeat': 'no-repeat',
+					'background-position': 'left top',
+					'background-size': 'cover',
+					'background-attachment': 'fixed',
+					'background-type': 'image',
 				},
 				mobile: {
-					'background-image': '',
-					'background-repeat': '',
-					'background-position': '',
-					'background-size': '',
-					'background-attachment': '',
-					'background-type': '',
+					'background-image': 'https://pd.w.org/2022/03/4136228f92e073825.69069021-rotated.jpg',
+					'background-repeat': 'no-repeat',
+					'background-position': 'left top',
+					'background-size': 'cover',
+					'background-attachment': 'fixed',
+					'background-type': 'image',
+				},
+			},
+			'header-mobile-items': {
+				primary: {
+					primary_center: {
+						0: 'menu-1',
+					},
 				},
 			},
 		};
@@ -41,6 +50,18 @@ describe( 'Primary menu settings in the customizer', () => {
 			selector: '.ast-builder-menu-1 .main-header-menu .sub-menu',
 			property: 'background-image',
 		} ).cssValueToBe( `url("${ primaryMenuImage[ 'header-menu1-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }` );
+
+		await setBrowserViewport( 'medium' );
+		await expect( {
+			selector: '.ast-builder-menu-1 .main-header-menu .sub-menu',
+			property: 'background-image',
+		} ).cssValueToBe( `url("${ primaryMenuImage[ 'header-menu1-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }` );
+
+		await setBrowserViewport( 'small' );
+		await expect( {
+			selector: '.ast-builder-menu-1 .main-header-menu .sub-menu',
+			property: 'background-image',
+		} ).cssValueToBe( `url("${ primaryMenuImage[ 'header-menu1-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }` );
 	} );
 } );
 

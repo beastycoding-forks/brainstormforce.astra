5b8661030dde81a1e15168de069ea10e86eb2b93
Simran <simranc@bsf.io>
2022-05-04 12:47:09 +0530

    changed selectors and modified code

--- tests/e2e/specs/customizer/global/typography/preset6.test.js
+++ tests/e2e/specs/customizer/global/typography/preset6.test.js
@@ -1,99 +1,95 @@
 import { createURL, createNewPost, setPostContent } from '@wordpress/e2e-test-utils';
-import { publishPost } from '../../../../utils/publish-post';
 import { setCustomize } from '../../../../utils/customize';
+import { publishPost } from '../../../../utils/publish-post';
 import { TPOGRAPHY_TEST_POST_CONTENT } from '../../../../utils/post';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
-describe( 'Global typography settings in the customizer', () => {
-	it( 'preset 6 settings should be applied correctly', async () => {
-		const presetFont = {
-			'body-font-family': "'Source Sans Pro', sans-serif",
+describe( 'Global typography preset-6 style in the customizer', () => {
+	it( 'body style should be applied correctly', async () => {
+		const globalTypographyPreset6 = {
+			'typography-presets': 'Preset6',
+			'body-font-family': "'Roboto,sans-serif'",
+			'body-font-weight': '400',
+			'body-text-transform': 'lowercase',
+			'body-line-height': '25px',
 			'font-size-body': {
-				desktop: '35',
-				tablet: '25',
+				desktop: '30',
+				tablet: '20',
 				mobile: '15',
 				'desktop-unit': 'px',
 				'tablet-unit': 'px',
 				'mobile-unit': 'px',
 			},
-			'body-font-weight': '400',
-			'body-text-transform': 'uppercase',
-			'body-line-height': '1.8',
-			'headings-font-family': 'Montserrat, sans-serif',
-			'headings-font-weight': '700',
-			'headings-text-transform': 'lowercase',
-			'headings-line-height': '2',
-			'font-size-h1': {
-				desktop: '40',
-				'desktop-unit': 'px',
-			},
 		};
-
-		await setCustomize( presetFont );
+		await setCustomize( globalTypographyPreset6 );
 		let ppStatus = false;
 		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'post', title: 'Preset-4-test' } );
+			await createNewPost( { postType: 'post', title: 'preset-6' } );
 			await setPostContent( TPOGRAPHY_TEST_POST_CONTENT );
 			ppStatus = await publishPost();
 		}
-		await page.goto( createURL( '/preset-4-test/' ), {
+		await page.goto( createURL( '/preset-6' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( 'body' );
 		await expect( {
 			selector: 'body',
 			property: 'font-family',
-		} ).cssValueToBe( `${ presetFont[ 'body-font-family' ] }` );
-
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'body-font-family' ] }` );
+		await expect( {
+			selector: 'body, button, input, select, textarea, .ast-button, .ast-custom-button',
+			property: 'font-weight',
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'body-font-weight' ] }` );
+		await expect( {
+			selector: 'body',
+			property: 'text-transform',
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'body-text-transform' ] }` );
+		await expect( {
+			selector: 'body, button, input, select, textarea, .ast-button, .ast-custom-button',
+			property: 'line-height',
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'body-line-height' ] }` );
 		await expect( {
 			selector: 'body',
 			property: 'font-size',
-		} ).cssValueToBe( `${ presetFont[ 'font-size-body' ].desktop }${ presetFont[ 'font-size-body' ][ 'desktop-unit' ] }` );
-
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'font-size-body' ].desktop }${ globalTypographyPreset6[ 'font-size-body' ][ 'desktop-unit' ] }` );
 		await setBrowserViewport( 'medium' );
 		await expect( {
 			selector: 'body',
 			property: 'font-size',
-		} ).cssValueToBe( `${ presetFont[ 'font-size-body' ].tablet }${ presetFont[ 'font-size-body' ][ 'tablet-unit' ] }` );
-
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'font-size-body' ].tablet }${ globalTypographyPreset6[ 'font-size-body' ][ 'tablet-unit' ] }` );
 		await setBrowserViewport( 'small' );
 		await expect( {
 			selector: 'body',
 			property: 'font-size',
-		} ).cssValueToBe( `${ presetFont[ 'font-size-body' ].mobile }${ presetFont[ 'font-size-body' ][ 'mobile-unit' ] }` );
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'font-size-body' ].mobile }${ globalTypographyPreset6[ 'font-size-body' ][ 'mobile-unit' ] }` );
+	} );
 
+	it( 'heading style should be applied correctly', async () => {
+		const globalTypographyPreset6 = {
+			'headings-font-family': "'Barlow Semi Condensed,sans-serif'",
+			'headings-font-weight': '600',
+			'headings-text-transform': 'uppercase',
+			'headings-line-height': '40px',
+		};
+		await setCustomize( globalTypographyPreset6 );
+		await page.goto( createURL( 'preset-6' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( 'h1, h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6' );
 		await expect( {
-			selector: 'body',
-			property: 'font-weight',
-		} ).cssValueToBe( `${ presetFont[ 'body-font-weight' ] }` );
-
-		await expect( {
-			selector: 'body',
-			property: 'text-transform',
-		} ).cssValueToBe( `${ presetFont[ 'body-text-transform' ] }` );
-
-		await expect( {
-			selector: 'body',
-			property: 'line-height',
-		} ).cssValueToBe( `${ presetFont[ 'body-line-height' ] * presetFont[ 'font-size-body' ].mobile }` + 'px' );
-
-		await expect( {
-			selector: '.entry-content h1',
+			selector: 'h1, h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6',
 			property: 'font-family',
-		} ).cssValueToBe( `${ presetFont[ 'headings-font-family' ] }` );
-
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'headings-font-family' ] }` );
 		await expect( {
-			selector: '.entry-content h1',
+			selector: 'h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6',
 			property: 'font-weight',
-		} ).cssValueToBe( `${ presetFont[ 'headings-font-weight' ] }` );
-
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'headings-font-weight' ] }` );
 		await expect( {
-			selector: '.entry-content h1',
+			selector: 'h1, h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6',
 			property: 'text-transform',
-		} ).cssValueToBe( `${ presetFont[ 'headings-text-transform' ] }` );
-
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'headings-text-transform' ] }` );
 		await expect( {
-			selector: '.entry-content h1',
+			selector: 'h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6',
 			property: 'line-height',
-		} ).cssValueToBe( `${ presetFont[ 'headings-line-height' ] * presetFont[ 'font-size-h1' ].desktop }` + 'px' );
+		} ).cssValueToBe( `${ globalTypographyPreset6[ 'headings-line-height' ] }` );
 	} );
 } );

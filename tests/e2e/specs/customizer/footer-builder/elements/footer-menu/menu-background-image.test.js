e1c0fc3de6a0a9ef739e10d6940b8d77d43aabff
Simran <simranc@bsf.io>
2022-05-09 15:29:48 +0530

    added E2E test case for footer menu background image

---
+++ tests/e2e/specs/customizer/footer-builder/elements/footer-menu/menu-background-image.test.js
@@ -0,0 +1,70 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../../utils/customize';
+import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
+import { scrollToElement } from '../../../../../utils/scroll-to-element';
+import { createNewFooterMenu } from '../../../../../utils/create-footer-menu';
+describe( 'Footer menu background image settings in the customizer', () => {
+	it( 'footer menu backgeound image should apply correctly', async () => {
+		await createNewFooterMenu();
+		const footerMenuBackgroundImage = {
+			'footer-menu-bg-obj-responsive': {
+				desktop: {
+					'background-color': 'https://pd.w.org/2022/03/3146230b053848c21.81950886-300x200.jpeg',
+					'background-repeat': 'no-repeat',
+					'background-position': 'left top',
+					'background-size': 'contain',
+					'background-attachment': 'fixed',
+					'background-type': 'image',
+				},
+				tablet: {
+					'background-image': 'https://pd.w.org/2022/03/3146230b053848c21.81950886-300x200.jpeg',
+					'background-repeat': 'no-repeat',
+					'background-position': 'left top',
+					'background-size': 'contain',
+					'background-attachment': 'fixed',
+					'background-type': 'image',
+				},
+				mobile: {
+					'background-color': 'https://pd.w.org/2022/03/6462309002a5de46.79671062-300x200.jpg',
+					'background-repeat': 'no-repeat',
+					'background-position': 'left top',
+					'background-size': 'contain',
+					'background-attachment': 'fixed',
+					'background-type': 'image',
+				},
+				'footer-desktop-items': {
+					primary: {
+						primary_2: {
+							0: 'menu',
+						},
+					},
+				},
+			},
+		};
+		await setCustomize( footerMenuBackgroundImage );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '#astra-footer-menu' );
+		await expect( {
+			selector: '#astra-footer-menu',
+			property: 'background-color',
+		} ).cssValueToBe( `${ footerMenuBackgroundImage[ 'footer-menu-bg-obj-responsive' ].desktop[ 'background-color' ] }` );
+
+		await setBrowserViewport( 'medium' );
+		await scrollToElement( '#colophon' );
+		await expect( {
+			selector: '#astra-footer-menu',
+			property: 'background-color',
+		} ).cssValueToBe( `${ footerMenuBackgroundImage[ 'footer-menu-bg-obj-responsive' ].tablet[ 'background-color' ] }` );
+
+		await setBrowserViewport( 'small' );
+		await scrollToElement( '#colophon' );
+		await expect( {
+			selector: '#astra-footer-menu',
+			property: 'background-color',
+		} ).cssValueToBe( `${ footerMenuBackgroundImage[ 'footer-menu-bg-obj-responsive' ].mobile[ 'background-color' ] }` );
+	} );
+} );

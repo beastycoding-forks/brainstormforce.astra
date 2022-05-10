d62b4e375e05a3ce32854c38950180706712c55c
Snehal Kolhe <snehalk@bsf.io>
2022-05-10 11:26:20 +0530

    changed selectors

--- tests/play/paragraph-block.spec.js
+++ tests/play/paragraph-block.spec.js
@@ -2,22 +2,23 @@
 /* eslint-disable jest/no-done-callback */
 const { test, expect } = require( '@playwright/test' );
 const { loginToSite } = require( './utils/login' );
-//const { createNewPost } = require( '../play/utils/create-new-post' );
 test.describe( 'Login to site', () => {
 	test( 'Setting values', async ( { page } ) => {
 		const login = new loginToSite( page );
 		await login.loginAsAdmin();
 
 		await page.goto( '/wp-admin/post-new.php' );
-		await page.locator( '#editor .block-editor-block-list__block' ).click();
-		await page.locator( '#editor .block-editor-block-list__block' ).type( 'sample' );
+		//await page.pause();
+		await page.locator( 'text=Type / to choose a block' ).click();
+		await page.locator( '[aria-label="Add title"]' ).click();
+		await page.locator( '[aria-label="Add title"]' ).type( 'sample' );
 		await page.locator( '.edit-post-header-toolbar .edit-post-header-toolbar__inserter-toggle.has-icon' ).click();
-		await page.locator( '#components-search-control-1' )
-			.type( 'Paragraph' );
-		await page.locator( '.components-button.block-editor-block-types-list__item' )
-			.click();
-
-		await page.locator( '.edit-post-visual-editor p' )
+		await page.locator( '[placeholder="Search"]' ).click();
+		await page.locator( '[placeholder="Search"]' ).type( 'Paragraph' );
+		await page.locator( '[aria-label="Empty block\\; start writing or type forward slash to choose a block"]' )
+			.first().click();
+		await page.locator( '[aria-label="Empty block\\; start writing or type forward slash to choose a block"]' )
+			.first()
 			.type( 'paragraph-block' );
 		const locator = page.locator( '.editor-styles-wrapper .block-editor-block-list__layout.is-root-container > p' );
 		await expect( locator )

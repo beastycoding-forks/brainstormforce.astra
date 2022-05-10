f6d93d30060fc3f9a8f71d5bd3258772980e0acd
Snehal Kolhe <snehalk@bsf.io>
2022-05-10 09:39:23 +0530

    fixed code analysis

--- tests/play/paragraph-block.spec.js
+++ tests/play/paragraph-block.spec.js
@@ -1,14 +1,10 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable jest/no-done-callback */
 const { test, expect } = require( '@playwright/test' );
 const { loginToSite } = require( './utils/login' );
 //const { createNewPost } = require( '../play/utils/create-new-post' );
 test.describe( 'Login to site', () => {
-		// test.beforeAll( async ( { request } ) => {
-		// 	// Reset setup
-		// 	const response = await request.delete( '/wp-json/astra/v1/e2e-utils/reset-site' );
-		// 	expect( response.ok() );
-		// } );
-
-		test( 'Setting values', async ( { page } ) => {
+	test( 'Setting values', async ( { page } ) => {
 		const login = new loginToSite( page );
 		await login.loginAsAdmin();
 
@@ -17,14 +13,14 @@ test.describe( 'Login to site', () => {
 		await page.locator( '#editor .block-editor-block-list__block' ).type( 'sample' );
 		await page.locator( '.edit-post-header-toolbar .edit-post-header-toolbar__inserter-toggle.has-icon' ).click();
 		await page.locator( '#components-search-control-1' )
-		.type('Paragraph');
+			.type( 'Paragraph' );
 		await page.locator( '.components-button.block-editor-block-types-list__item' )
-		.click();
+			.click();
 
 		await page.locator( '.edit-post-visual-editor p' )
-		.type('paragraph-block');
-		const locator= page.locator( '.editor-styles-wrapper .block-editor-block-list__layout.is-root-container > p' );
-		await expect(locator)
-		.toHaveCSS( 'max-width', '1200px' );
-	});
-});
+			.type( 'paragraph-block' );
+		const locator = page.locator( '.editor-styles-wrapper .block-editor-block-list__layout.is-root-container > p' );
+		await expect( locator )
+			.toHaveCSS( 'max-width', '1200px' );
+	} );
+} );

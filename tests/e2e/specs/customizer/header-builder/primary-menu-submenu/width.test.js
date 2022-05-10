c635c0b629991b00f53557208df90ffebd11f0de
Simran <simranc@bsf.io>
2022-05-10 13:53:17 +0530

    added waitForSelector()

--- tests/e2e/specs/customizer/header-builder/primary-menu-submenu/width.test.js
+++ tests/e2e/specs/customizer/header-builder/primary-menu-submenu/width.test.js
@@ -19,6 +19,7 @@ describe( 'Primary menu submenu option under the customizer', () => {
 			waitUntil: 'networkidle0',
 		} );
 		await page.hover( '.menu-link' );
+		await page.waitForSelector( '.ast-builder-menu-1 .sub-menu' );
 		await expect( {
 			selector: '.ast-builder-menu-1 .sub-menu',
 			property: 'width',

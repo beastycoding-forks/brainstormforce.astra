8ead9f6eb63b09e2df90c66f50200ac3d0ac6474
Snehal Kolhe <snehalk@bsf.io>
2022-05-11 11:56:43 +0530

    resolved issue and modified test case

--- tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/inner-element-space.test.js
+++ tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/inner-element-space.test.js
@@ -1,31 +1,29 @@
-import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
-import { publishPost } from '../../../../../utils/publish-post';
+import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../../utils/customize';
 import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
+import { createNewMenu } from '../../../../../utils/create-menu';
 describe( 'Off canvas full-screen header type popup padding setting in the customizer', () => {
 	it( 'padding should apply correctly', async () => {
 		const innerElementSpace = {
 			'header-mobile-items': {
 				popup: {
 					popup_content: {
-						0: 'account',
-						1: 'social-icons-1',
+						0: 'mobile-menu',
+						1: 'button-1',
 						2: 'search',
 					},
 				},
+				primary: {
+					primary_right: {
+						0: 'mobile-trigger',
+					},
+				},
 			},
-			'off-canvas-inner-spacing': '9',
+			'off-canvas-inner-spacing': 9,
 			'mobile-header-type': 'full-width',
 		};
 		await setCustomize( innerElementSpace );
-		let ppStatus = false;
-		while ( false === ppStatus ) {
-			await createNewPost( {
-				postType: 'post',
-				title: 'sample-page',
-			} );
-			ppStatus = await publishPost();
-		}
+		await createNewMenu();
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -34,7 +32,7 @@ describe( 'Off canvas full-screen header type popup padding setting in the custo
 		await page.waitForSelector( '.ast-mobile-popup-content > *' );
 		await expect( {
 			selector: '.ast-mobile-popup-content > *',
-			property: 'padding-top',
+			property: 'padding-bottom',
 		} ).cssValueToBe(
 			`${ innerElementSpace[ 'off-canvas-inner-spacing' ] + 'px' }`,
 		);

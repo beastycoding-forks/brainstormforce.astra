1fe9d0be07f8449c48c8a26eac6521d5553f787c
Simran <simranc@bsf.io>
2022-05-11 12:25:58 +0530

    E2E test case for astra meta setting disable footer option

---
+++ tests/e2e/specs/customizer/astra-meta-settings/disable-footer.test.js
@@ -0,0 +1,38 @@
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
+import { publishPost } from '../../../utils/publish-post';
+import { setCustomize } from '../../../utils/customize';
+import { setBrowserViewport } from '../../../utils/set-browser-viewport';
+import { scrollToElement } from '../../../utils/scroll-to-element';
+describe( 'disable footer meta setting', () => {
+	it( 'disabling footer should work properly', async () => {
+		const astraMetaSetting = {
+			'footer-sml-layout': 'disabled',
+			'footer-desktop-items': {
+				primary: {
+					primary_2: {
+						1: 'social-icons-1',
+					},
+				},
+			},
+		};
+		await setCustomize( astraMetaSetting );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'post', title: 'test' } );
+			await page.click( '[aria-label="Astra Settings"]' );
+			await page.click( '#astra_settings_meta_box > div:nth-child(3) > h2 > button' );
+			await page.evaluate( () => {
+				[ ...document.querySelectorAll( '.ast-sidebar-layout-meta-wrap .components-toggle-control__label' ) ].find( ( element ) => element.textContent === 'Disable Footer' ).click();
+			} );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/test' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#secondary' );
+		await page.waitForSelector( '#secondary' );
+		const siteFooter = await page.$eval( '#secondary', ( element ) => element.getAttribute( 'colophon' ) );
+		await expect( siteFooter ).toBeNull( );
+	} );
+} );

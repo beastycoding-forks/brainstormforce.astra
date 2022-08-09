291557a08e62f718c90b87c055b0d0eea3d254b0
Yogesh Sambare <bloggingscan@gmail.com>
2022-08-09 19:59:07 +0530

    fix:auto zoom are breaking alignment on IOS devices

--- header.php
+++ header.php
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <head>
 <?php astra_head_top(); ?>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="profile" href="https://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>

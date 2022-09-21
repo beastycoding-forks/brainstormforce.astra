3e68340cb25cb56e1c471fb75afab560adc6c2ea
Gaurav Khupse <44017457+GauravKhupse@users.noreply.github.com>
2022-09-21 17:30:45 +0530

    sticky add to cart is not working on old header

--- inc/core/class-astra-enqueue-scripts.php
+++ inc/core/class-astra-enqueue-scripts.php
@@ -189,16 +189,20 @@ if ( ! class_exists( 'Astra_Enqueue_Scripts' ) ) {
 					$default_assets['js']['astra-mobile-cart'] = 'mobile-cart';
 				}
 
-				if ( class_exists( 'WooCommerce' ) && is_product() && astra_get_option( 'single-product-sticky-add-to-cart' ) ) {
-					$default_assets['js']['astra-sticky-add-to-cart'] = 'sticky-add-to-cart';
-				}
-
 				/** @psalm-suppress UndefinedFunction */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 				$astra_add_to_cart_quantity_btn_enabled = apply_filters( 'astra_add_to_cart_quantity_btn_enabled', astra_get_option( 'single-product-plus-minus-button' ) );
 
 				if ( class_exists( 'WooCommerce' ) && $astra_add_to_cart_quantity_btn_enabled ) {
 					$default_assets['js']['astra-add-to-cart-quantity-btn'] = 'add-to-cart-quantity-btn';
 				}
+			}
+			$astra_add_to_cart_quantity_btn_enabled = apply_filters( 'astra_add_to_cart_quantity_btn_enabled', astra_get_option( 'single-product-plus-minus-button' ) );
+
+			if ( class_exists( 'WooCommerce' ) && $astra_add_to_cart_quantity_btn_enabled ) {
+					$default_assets['js']['astra-add-to-cart-quantity-btn'] = 'add-to-cart-quantity-btn';
+			}
+			if ( class_exists( 'WooCommerce' ) && is_product() && astra_get_option( 'single-product-sticky-add-to-cart' ) ) {
+				$default_assets['js']['astra-sticky-add-to-cart'] = 'sticky-add-to-cart';
 			}
 			return apply_filters( 'astra_theme_assets', $default_assets );
 		}

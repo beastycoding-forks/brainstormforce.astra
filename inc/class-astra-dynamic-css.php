977407320b0696a56680026bb21333bc7a23af01
neerajmasai <neerajmalwal10@gmail.com>
2022-11-10 15:48:50 +0530

    stash

--- inc/class-astra-dynamic-css.php
+++ inc/class-astra-dynamic-css.php
@@ -3040,7 +3040,7 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 			if( 'narrow-container' === $container_layout ) {
 
 				$narrow_container_css = array(
-					'.ast-narrow-container .site-content > .ast-container' => array(
+					'.ast-narrow-container .ast-container' => array(
 						'max-width' => astra_get_css_value( $narrow_container_max_width, 'px' ),
 					)
 				);

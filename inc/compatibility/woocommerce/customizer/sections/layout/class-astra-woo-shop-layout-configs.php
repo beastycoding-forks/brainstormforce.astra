37dae67a616d709d327ad8700420ade90a89a583
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-11-16 09:15:34 +0530

    fix : add to cart action conditions

--- inc/compatibility/woocommerce/customizer/sections/layout/class-astra-woo-shop-layout-configs.php
+++ inc/compatibility/woocommerce/customizer/sections/layout/class-astra-woo-shop-layout-configs.php
@@ -38,12 +38,17 @@ if ( ! class_exists( 'Astra_Woo_Shop_Layout_Configs' ) ) {
 			$add_to_cart_attr             = array();
 			$astra_shop_page_pro_features = array();
 
+
+			
 			if ( 'yes' === $is_ajax_add_to_cart_enabled ) {
 				
-				$astra_shop_page_pro_features = array(
-					'redirect_cart_page'     => __( 'Redirect To Cart Page', 'astra' ),
-					'redirect_checkout_page' => __( 'Redirect To Checkout Page', 'astra' ),
-				);
+				if ( $astra_addon_with_woo ) {
+					$astra_shop_page_pro_features = array(
+						'redirect_cart_page'     => __( 'Redirect To Cart Page', 'astra' ),
+						'redirect_checkout_page' => __( 'Redirect To Checkout Page', 'astra' ),
+					);
+				}
+			
 
 				/**
 				 * Shop product add to cart control.
@@ -54,6 +59,10 @@ if ( ! class_exists( 'Astra_Woo_Shop_Layout_Configs' ) ) {
 					'main_index'  => 'add_cart',
 					'clone_limit' => 2,
 					'title'       => __( 'Add To Cart', 'astra' ),
+				);
+			} else {
+				$add_to_cart_attr = array(
+					'add_cart' => __( 'Add To Cart', 'astra' ),
 				);
 			}
 
@@ -335,7 +344,6 @@ if ( ! class_exists( 'Astra_Woo_Shop_Layout_Configs' ) ) {
 					'renderAs'   => 'text',
 				);
 			}
-		
 
 			// Learn More link if Astra Pro is not activated.
 			if ( astra_showcase_upgrade_notices() ) {

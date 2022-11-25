f7cbdffc842b7c7273d40fb68f53b975e05065eb
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-11-25 11:28:28 +0530

    fix : phpcs

--- inc/compatibility/woocommerce/assets/js/unminified/shop-add-to-cart.js
+++ inc/compatibility/woocommerce/assets/js/unminified/shop-add-to-cart.js
@@ -1,19 +1,22 @@
 jQuery( document ).ready(function() {
 	jQuery( document.body ).on( 'added_to_cart', function() {
+
 		const slideInCart = jQuery( '#astra-mobile-cart-drawer' );
 
-		if( 'slide_in_cart' === astraShopAddToCart.shop_add_to_cart_action ) {
-			slideInCart.addClass( 'active' );
-			jQuery( 'html' ).addClass( 'ast-mobile-cart-active' );
-		}
-		if( astraShopAddToCart.is_astra_pro ) {
-
-			if( 'redirect_cart_page' === astraShopAddToCart.shop_add_to_cart_action ) {
-				window.open( astraShopAddToCart.cart_url ,"_self");
+		if( astraShopAddToCart.shop_add_to_cart_action ) {
+			if( 'slide_in_cart' === astraShopAddToCart.shop_add_to_cart_action ) {
+				slideInCart.addClass( 'active' );
+				jQuery( 'html' ).addClass( 'ast-mobile-cart-active' );
 			}
-
-			if( 'redirect_checkout_page' === astraShopAddToCart.shop_add_to_cart_action ) {
-				window.open( astraShopAddToCart.checkout_url ,"_self");
+			if( astraShopAddToCart.is_astra_pro ) {
+	
+				if( 'redirect_cart_page' === astraShopAddToCart.shop_add_to_cart_action ) {
+					window.open( astraShopAddToCart.cart_url ,"_self");
+				}
+	
+				if( 'redirect_checkout_page' === astraShopAddToCart.shop_add_to_cart_action ) {
+					window.open( astraShopAddToCart.checkout_url ,"_self");
+				}
 			}
 		}
 

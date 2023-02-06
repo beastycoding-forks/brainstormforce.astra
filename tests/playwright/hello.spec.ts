129bb1a2a9b9368cdeb9574dabac684cedce6b61
Simran <simranc@bsf.io>
2023-02-06 15:32:53 +0530

    added docker start and stop environment in github workflow

--- tests/playwright/hello.spec.ts
+++ tests/playwright/hello.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('hello world', async ({ page }) => {
   page.setDefaultTimeout(0);
   await page.goto('/?p=1');
-
+  page.setDefaultTimeout(500);
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Hello/);
 });

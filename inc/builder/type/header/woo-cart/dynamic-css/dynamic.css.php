4f7d885a6a2d6ab6dc36ff6e60f410b9bd736644
Brainstorm Force <brainstormforce@Garvits-MacBook-Pro.local>
2023-12-08 15:35:51 +0545

    Code comments improvements for PHPCS test

--- inc/builder/type/header/woo-cart/dynamic-css/dynamic.css.php
+++ inc/builder/type/header/woo-cart/dynamic-css/dynamic.css.php
@@ -521,12 +521,12 @@ function astra_hb_woo_cart_dynamic_css( $dynamic_css, $dynamic_css_filtered = ''
 				'color' => esc_attr( $icon_color ),
 			),
 
-			// Fix: Count color for the legacy users. ( shopping-default ) [Ref: #AST-3683]
+			// Fix: Count color for the legacy users. ( shopping-default ) [Ref: #AST-3683].
 			'.ast-menu-cart-outline .ast-cart-menu-wrap .count' => array(
 				'color' => esc_attr( $cart_products_count_color ),
 			),
 
-			// Fix: Count hover color for the legacy users. ( shopping-default ) [Ref: #AST-3683]
+			// Fix: Count hover color for the legacy users. ( shopping-default ) [Ref: #AST-3683].
 			'.ast-menu-cart-outline .ast-cart-menu-wrap .count:hover' => array(
 				'color' => esc_attr( $cart_products_count_color_hover ),
 			),
